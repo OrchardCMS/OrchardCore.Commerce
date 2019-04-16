51ba86c78e09a981db255062eb98d453a1bc0275
Magnus Pettersson <magnus.pettersson@micropos.nu>
2019-04-16 20:28:54 +0200

    Added SwedishKrona as currency. Changed to display IsoCode in PricePart editor. Many currencies have the same symbol, so it's not clear what currency is selected.

--- Money/CurrencyProvider.cs
+++ Money/CurrencyProvider.cs
@@ -9,7 +9,7 @@ namespace OrchardCore.Commerce.Money
     {
         private static readonly IEnumerable<ICurrency> _currencies = new List<ICurrency> {
             Dollar, Euro, Yen, PoundSterling, AustralianDollar,
-            CanadianDollar, SwissFranc, Renminbi, Currency.BitCoin
+            CanadianDollar, SwissFranc, Renminbi, SwedishKrona, Currency.BitCoin
         };
         private static readonly IDictionary<string, ICurrency> _currencyDictionary
             = _currencies.ToDictionary(c => c.IsoCode);
