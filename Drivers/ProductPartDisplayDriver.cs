e05e6f6e186486668da873afdeeff0358a453e17
Magnus Pettersson <magnus.pettersson@micropos.nu>
2020-02-14 16:55:31 +0100

    Refactor ProductPart to use overloads that brings the context.

--- Drivers/ProductPartDisplayDriver.cs
+++ Drivers/ProductPartDisplayDriver.cs
@@ -3,6 +3,7 @@ using OrchardCore.Commerce.Abstractions;
 using OrchardCore.Commerce.Models;
 using OrchardCore.Commerce.ViewModels;
 using OrchardCore.ContentManagement.Display.ContentDisplay;
+using OrchardCore.ContentManagement.Display.Models;
 using OrchardCore.DisplayManagement.ModelBinding;
 using OrchardCore.DisplayManagement.Views;
 
@@ -17,26 +18,23 @@ namespace OrchardCore.Commerce.Drivers
             _productAttributeService = productAttributeService;
         }
 
-        public override IDisplayResult Display(ProductPart productPart)
+        public override IDisplayResult Display(ProductPart productPart, BuildPartDisplayContext context)
         {
-            return Combine(
-                Initialize<ProductPartViewModel>("ProductPart", m => BuildViewModel(m, productPart))
-                    .Location("Detail", "Content:20"),
-                Initialize<ProductPartViewModel>("ProductPart_Summary", m => BuildViewModel(m, productPart))
-                    .Location("Summary", "Meta:5")
-            );
+            return Initialize<ProductPartViewModel>(GetDisplayShapeType(context), m => BuildViewModel(m, productPart))
+                .Location("Detail", "Content:20")
+                .Location("Summary", "Meta:5");
         }
 
-        public override IDisplayResult Edit(ProductPart productPart)
+        public override IDisplayResult Edit(ProductPart productPart, BuildPartEditorContext context)
         {
-            return Initialize<ProductPartViewModel>("ProductPart_Edit", m => BuildViewModel(m, productPart));
+            return Initialize<ProductPartViewModel>(GetEditorShapeType(context), m => BuildViewModel(m, productPart));
         }
 
-        public override async Task<IDisplayResult> UpdateAsync(ProductPart model, IUpdateModel updater)
+        public override async Task<IDisplayResult> UpdateAsync(ProductPart model, IUpdateModel updater, UpdatePartEditorContext context)
         {
             await updater.TryUpdateModelAsync(model, Prefix, t => t.Sku);
 
-            return Edit(model);
+            return Edit(model, context);
         }
 
         private Task BuildViewModel(ProductPartViewModel model, ProductPart part)
