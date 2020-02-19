db4ae2988035a99fc7c3c14868fcaf63a9501a2e
Magnus Pettersson <magnus.pettersson@micropos.nu>
2020-02-19 21:55:30 +0100

    Update to use new extension method in Orchard Core

--- Services/PriceProvider.cs
+++ Services/PriceProvider.cs
@@ -4,7 +4,6 @@ using System.Threading.Tasks;
 using OrchardCore.Commerce.Abstractions;
 using OrchardCore.Commerce.Models;
 using OrchardCore.ContentManagement;
-using OrchardCore.ContentManagement.Metadata;
 
 namespace OrchardCore.Commerce.Services
 {
@@ -15,19 +14,13 @@ namespace OrchardCore.Commerce.Services
     {
         private readonly IProductService _productService;
         private readonly IMoneyService _moneyService;
-        private readonly IContentDefinitionManager _contentDefinitionManager;
-        private readonly ITypeActivatorFactory<ContentPart> _contentPartFactory;
 
         public PriceProvider(
             IProductService productService,
-            IMoneyService moneyService,
-            IContentDefinitionManager contentDefinitionManager,
-            ITypeActivatorFactory<ContentPart> contentPartFactory)
+            IMoneyService moneyService)
         {
             _productService = productService;
             _moneyService = moneyService;
-            _contentDefinitionManager = contentDefinitionManager;
-            _contentPartFactory = contentPartFactory;
         }
 
         public int Order => 0;
@@ -41,47 +34,13 @@ namespace OrchardCore.Commerce.Services
             {
                 if (skuProducts.TryGetValue(item.ProductSku, out var product))
                 {
-                    ContentItem contentItem = product.ContentItem;
+                    var contentItem = product.ContentItem;
 
-                    // TODO: Swap the call below to extension method on ContentItem if approved in Orchard Core.
-                    //       Also Remove the temporary implementation of PartsOfType and dependencies on ContentDefinitionManager
-                    foreach (var pricePart in PartsOfType<PricePart>(contentItem))
+                    foreach (var pricePart in contentItem.OfType<PricePart>()
+                                 .Where(p => p.Price.Currency == _moneyService.CurrentDisplayCurrency))
                     {
-                        if (pricePart.Price.Currency == _moneyService.CurrentDisplayCurrency)
-                        {
-                            item.Prices.Add(new PrioritizedPrice(0, pricePart.Price));
-                        }
+                        item.Prices.Add(new PrioritizedPrice(0, pricePart.Price));
                     }
-
-                    //foreach (var pricePart in contentItem.OfType<PricePart>()
-                    //             .Where(p => p.Price.Currency == _moneyService.CurrentDisplayCurrency))
-                    //{
-                    //    item.Prices.Add(new PrioritizedPrice(0, pricePart.Price));
-                    //}
-                }
-            }
-        }
-
-        /// <summary>
-        /// Gets all content elements of a specific type.
-        /// </summary>
-        /// <typeparam name="TPart"></typeparam>
-        /// <param name="contentItem"></param>
-        /// <returns></returns>
-        public IEnumerable<TPart> PartsOfType<TPart>(ContentItem contentItem) where TPart : ContentPart
-        {
-            var contentTypeDefinition = _contentDefinitionManager.GetTypeDefinition(contentItem.ContentType);
-
-            foreach (var contentTypePartDefinition in contentTypeDefinition.Parts)
-            {
-                var partName = contentTypePartDefinition.Name;
-                var partTypeName = contentTypePartDefinition.PartDefinition.Name;
-                var partActivator = _contentPartFactory.GetTypeActivator(partTypeName);
-                var part = contentItem.Get(partActivator.Type, partName) as TPart;
-
-                if (part != null)
-                {
-                    yield return part;
                 }
             }
         }
