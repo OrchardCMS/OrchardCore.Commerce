63342e2ae38f208489bb9bc108582cfbe6fbe4d2
Jasmin Savard <jasmin.savard@gmail.com>
2021-06-05 11:40:50 -0400

    Add ECommerce theme

---
+++ src/OrchardCore.Themes/TheEcommerce/Manifest.cs
@@ -0,0 +1,11 @@
+using OrchardCore.DisplayManagement.Manifest;
+using OrchardCore.Modules.Manifest;
+
+[assembly: Theme(
+    Name = "The Ecommerce Theme",
+    Author = ManifestConstants.OrchardCoreTeam,
+    Website = ManifestConstants.OrchardCoreWebsite,
+    Version = ManifestConstants.OrchardCoreVersion,
+    Description = "The Ecommerce Theme.",
+    Tags = new[] { "Bootstrap", "Default" }
+)]
