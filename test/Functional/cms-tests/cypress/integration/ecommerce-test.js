b7e97e9db311d8fd11d9097208c308a750d616c7
Jasmin Savard <jasmin.savard@gmail.com>
2021-06-05 12:02:09 -0400

    wip

--- test/Functional/cms-tests/cypress/integration/ecommerce-test.js
+++ test/Functional/cms-tests/cypress/integration/ecommerce-test.js
@@ -1,20 +1,20 @@
 /// <reference types="Cypress" />
 import { generateTenantInfo } from 'cypress-orchardcore/dist/utils';
 
-describe('Ecommerce Recipe test', function () {
+describe('ECommerce Recipe test', function () {
     let tenant;
 
     before(() => {
-        tenant = generateTenantInfo("Ecommerce")
+        tenant = generateTenantInfo("ECommerce")
         cy.newTenant(tenant);
     })
 
-    it('Displays the home page of the Ecommerce theme', function(){
+    it('Displays the home page of the ECommerce theme', function(){
         cy.visit(`${tenant.prefix}`);
         cy.get('h4').should('contain.text', 'Welcome to the Orchard Framework, your site has been successfully set up.');
     })
 
-    it('SaaS admin login should work', function(){
+    it('Ecommerce admin login should work', function(){
         cy.login(tenant);
         cy.visit(`${tenant.prefix}/Admin`);
         cy.get('.ta-content').should('contain.text', 'Welcome to Orchard')
