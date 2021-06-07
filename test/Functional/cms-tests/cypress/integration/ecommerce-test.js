a1ce8f9d6deab1765b4794ae49238d7d096d114f
Jasmin Savard <jasmin.savard@gmail.com>
2021-06-07 01:00:55 -0400

    Fix functional tests

--- test/Functional/cms-tests/cypress/integration/ecommerce-test.js
+++ test/Functional/cms-tests/cypress/integration/ecommerce-test.js
@@ -1,15 +1,15 @@
 /// <reference types="Cypress" />
 import { generateTenantInfo } from 'cypress-orchardcore/dist/utils';
 
-describe('ECommerce Recipe test', function () {
+describe('Ecommerce Recipe test', function () {
     let tenant;
 
     before(() => {
-        tenant = generateTenantInfo("ECommerce")
+        tenant = generateTenantInfo("Ecommerce")
         cy.newTenant(tenant);
     })
 
-    it('Displays the home page of the ECommerce theme', function(){
+    it('Displays the home page of the Ecommerce theme', function(){
         cy.visit(`${tenant.prefix}`);
         cy.get('h4').should('contain.text', 'Welcome to the Orchard Framework, your site has been successfully set up.');
     })
