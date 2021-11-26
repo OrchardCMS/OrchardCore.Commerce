3b5bdf1470adac771be5a3c2adc24a36575e07d1
jhgbrt <jeroen@haegebaert.com>
2021-11-27 00:58:59 +0100

    simplified Program.cs and removed Startup.cs (as per net5 -> net6 migration guidance)

--- SampleWebApp/Program.cs
+++ SampleWebApp/Program.cs
@@ -1,26 +1,24 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace SampleWebApp
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOrchardCms();
+
+var app = builder.Build();
+
+
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseOrchardCore();
+
+app.Run();
