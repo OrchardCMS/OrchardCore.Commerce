7402e8b103f6be8a9c9cfdd9ed8731e6129f0700
Dávid El-Saig <david.el-saig@lombiq.com>
2022-04-23 19:28:25 +0200

    Fix SampleWebApp.

--- SampleWebApp/Program.cs
+++ SampleWebApp/Program.cs
@@ -3,14 +3,11 @@ using Microsoft.Extensions.Hosting;
 
 namespace SampleWebApp;
 
-public class Program
+public static class Program
 {
     public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+            .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
 }
