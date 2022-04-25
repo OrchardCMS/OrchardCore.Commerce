e59a69afed19db574468f050056e5657a038c8b7
Dávid El-Saig <david.el-saig@lombiq.com>
2022-04-25 23:00:51 +0200

    Code styling.

--- OrchardCore.Commerce.Tests/Fakes/FakeContentDefinitionManager.cs
+++ OrchardCore.Commerce.Tests/Fakes/FakeContentDefinitionManager.cs
@@ -20,65 +20,69 @@ public class FakeContentDefinitionManager : IContentDefinitionManager
 
     public ContentPartDefinition GetPartDefinition(string name) => throw new NotSupportedException();
 
-    public ContentTypeDefinition GetTypeDefinition(string name)
-        => new(name, name, new[]
-        {
-            new ContentTypePartDefinition(
-                "ProductPart1",
-                new ContentPartDefinition(
-                    "ProductPartType",
-                    new[]
-                    {
-                        new ContentPartFieldDefinition(
-                            new ContentFieldDefinition(nameof(BooleanProductAttributeField)),
-                            "foobool",
-                            new JObject()),
-                        new ContentPartFieldDefinition(
-                            new ContentFieldDefinition(nameof(BooleanField)),
-                            "barbool",
-                            new JObject()),
-                    },
+    public ContentTypeDefinition GetTypeDefinition(string name) =>
+        new(
+            name,
+            name,
+            new[]
+            {
+                new ContentTypePartDefinition(
+                    "ProductPart1",
+                    new ContentPartDefinition(
+                        "ProductPartType",
+                        new[]
+                        {
+                            new ContentPartFieldDefinition(
+                                new ContentFieldDefinition(nameof(BooleanProductAttributeField)),
+                                "foobool",
+                                new JObject()),
+                            new ContentPartFieldDefinition(
+                                new ContentFieldDefinition(nameof(BooleanField)),
+                                "barbool",
+                                new JObject()),
+                        },
+                        new JObject()),
                     new JObject()),
-                new JObject()),
-            new ContentTypePartDefinition(
-                "ProductPart2",
-                new ContentPartDefinition(
-                    "ProductPartType2",
-                    new[]
-                    {
-                        new ContentPartFieldDefinition(
-                            new ContentFieldDefinition(nameof(TextProductAttributeField)),
-                            "footext",
-                            new JObject()),
-                        new ContentPartFieldDefinition(
-                            new ContentFieldDefinition(nameof(TextField)),
-                            "bartext",
-                            new JObject()),
-                    },
+                new ContentTypePartDefinition(
+                    "ProductPart2",
+                    new ContentPartDefinition(
+                        "ProductPartType2",
+                        new[]
+                        {
+                            new ContentPartFieldDefinition(
+                                new ContentFieldDefinition(nameof(TextProductAttributeField)),
+                                "footext",
+                                new JObject()),
+                            new ContentPartFieldDefinition(
+                                new ContentFieldDefinition(nameof(TextField)),
+                                "bartext",
+                                new JObject()),
+                        },
+                        new JObject()),
                     new JObject()),
-                new JObject()),
-            new ContentTypePartDefinition(
-                "ProductPart3",
-                new ContentPartDefinition(
-                    "product",
-                    new[]
-                    {
-                        new ContentPartFieldDefinition(
-                            new ContentFieldDefinition(nameof(BooleanProductAttributeField)),
-                            "attr1",
-                            new JObject()),
-                        new ContentPartFieldDefinition(
-                            new ContentFieldDefinition(nameof(TextProductAttributeField)),
-                            "attr2",
-                            new JObject()),
-                        new ContentPartFieldDefinition(
-                            new ContentFieldDefinition(nameof(NumericProductAttributeField)),
-                            "attr3",
-                            new JObject()),
-                    },
+                new ContentTypePartDefinition(
+                    "ProductPart3",
+                    new ContentPartDefinition(
+                        "product",
+                        new[]
+                        {
+                            new ContentPartFieldDefinition(
+                                new ContentFieldDefinition(nameof(BooleanProductAttributeField)),
+                                "attr1",
+                                new JObject()),
+                            new ContentPartFieldDefinition(
+                                new ContentFieldDefinition(nameof(TextProductAttributeField)),
+                                "attr2",
+                                new JObject()),
+                            new ContentPartFieldDefinition(
+                                new ContentFieldDefinition(nameof(NumericProductAttributeField)),
+                                "attr3",
+                                new JObject()),
+                        },
+                        new JObject()),
                     new JObject()),
-                new JObject()),
-        }, new JObject());
+            },
+            new JObject());
 
     public Task<int> GetTypesHashAsync() => throw new NotSupportedException();
     public IEnumerable<ContentPartDefinition> ListPartDefinitions() => throw new NotSupportedException();
