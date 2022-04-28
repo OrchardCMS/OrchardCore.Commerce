9c24fef4d964943e5a42e49651682e8520225d59
Dávid El-Saig <david.el-saig@lombiq.com>
2022-04-28 22:22:56 +0200

    Simplify expression.

--- AddressDataType/ConcatenationHelper.cs
+++ AddressDataType/ConcatenationHelper.cs
@@ -5,5 +5,5 @@ namespace InternationalAddress;
 public static class ConcatenationHelper
 {
     public static string JoinNotNullAndNotWhiteSpace(string separator, params string[] items) =>
-        string.Join(separator, items.Where(item => string.IsNullOrWhiteSpace(item)));
+        string.Join(separator, items.Where(string.IsNullOrWhiteSpace));
 }
