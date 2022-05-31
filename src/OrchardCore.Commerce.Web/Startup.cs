b4278dad0ab94795116c3df1625ae927c4d635c4
Roland Márkus <roland.markus@lombiq.com>
2022-05-31 19:57:40 +0200

    Adding recipe structure and renaming web app.

--- src/OrchardCore.Commerce.Web/Startup.cs
+++ src/OrchardCore.Commerce.Web/Startup.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace SampleWebApp;
+namespace OrchardCore.Commerce.Web;
 
 public class Startup
 {
