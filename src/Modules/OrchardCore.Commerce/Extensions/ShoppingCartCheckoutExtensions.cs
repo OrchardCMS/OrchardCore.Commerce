60255646e90c78ba311587aaf180e0fccb7bda7a
Szabolcs Deme <szabolcs.deme@lombiq.com>
2022-06-27 16:41:53 +0200

    Creating method that calculates total(s)

---
+++ src/Modules/OrchardCore.Commerce/Extensions/ShoppingCartCheckoutExtensions.cs
@@ -0,0 +1,28 @@
+using Money;
+using OrchardCore.Commerce.Abstractions;
+using OrchardCore.Commerce.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrchardCore.Commerce.Extensions;
+public static class ShoppingCartCheckoutExtensions
+{
+    public static async Task<IEnumerable<Amount>> CalculateTotalsAsync(
+        this ShoppingCart shoppingCart,
+        IPriceService priceService,
+        IPriceSelectionStrategy priceSelectionStrategy)
+    {
+        var items = await priceService.AddPricesAsync(shoppingCart.Items);
+        var lines = await Task.WhenAll(items.Select(item =>
+        {
+            var price = priceSelectionStrategy.SelectPrice(item.Prices);
+
+            return Task.FromResult(item.Quantity * price);
+        }));
+
+        return lines
+             .GroupBy(linePrice => linePrice.Currency)
+             .Select(group => new Amount(group.Sum(linePrice => linePrice.Value), group.Key));
+    }
+}
