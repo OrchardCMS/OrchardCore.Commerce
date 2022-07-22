e0b1909c9c5b5d87a562cc31207886de636a84a8
Szabolcs Deme <szabolcs.deme@lombiq.com>
2022-07-22 03:25:07 +0200

    Fixing tests

--- test/OrchardCore.Commerce.Tests/Fakes/FakePriceService.cs
+++ test/OrchardCore.Commerce.Tests/Fakes/FakePriceService.cs
@@ -12,12 +12,12 @@ public class FakePriceService : IPriceService
     public Task<IList<ShoppingCartItem>> AddPricesAsync(IList<ShoppingCartItem> items) =>
         Task.FromResult<IList<ShoppingCartItem>>(
             items
-                .Select((item, index) => item.WithPrice(new PrioritizedPrice(
-                    priority: 0,
-                    price: new Amount(42 + index, Currency.UsDollar))))
+                .Select((item, index) => AddPriceToShoppingCartItem(item, index))
                 .ToList());
 
     public Task<ShoppingCartItem> AddPriceAsync(ShoppingCartItem item) =>
-    Task.FromResult(
-        item.WithPrice(new PrioritizedPrice(priority: 0, price: new Amount(42, Currency.UsDollar))));
+        Task.FromResult(AddPriceToShoppingCartItem(item));
+
+    private static ShoppingCartItem AddPriceToShoppingCartItem(ShoppingCartItem item, int index = 0) =>
+        item.WithPrice(new PrioritizedPrice(priority: 0, price: new Amount(42 + index, Currency.UsDollar)));
 }
