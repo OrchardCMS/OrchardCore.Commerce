6c9d2d3522d05a4f57b8e3d77d2bbfc7dd28b861
Roland Márkus <roland.markus@lombiq.com>
2022-08-05 19:21:24 +0200

    Handler added.

---
+++ src/Modules/OrchardCore.Commerce/Handlers/UniqueSkuValidationHandler.cs
@@ -0,0 +1,42 @@
+﻿using Lombiq.HelpfulLibraries.OrchardCore.Contents;
+using OrchardCore.Commerce.Models;
+using OrchardCore.ContentManagement;
+using OrchardCore.ContentManagement.Handlers;
+using OrchardCore.DisplayManagement.ModelBinding;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YesSql;
+
+namespace OrchardCore.Commerce.Handlers;
+
+public class UniqueSkuValidationHandler : ContentPartHandler<ProductPart>
+{
+    private readonly IContentManager _contentManager;
+    private readonly ISession _session;
+    private readonly IUpdateModelAccessor _updateModelAccessor;
+
+    public UniqueSkuValidationHandler(
+        IContentManager contentManager,
+        ISession session,
+        IUpdateModelAccessor updateModelAccessor)
+    {
+        _contentManager = contentManager;
+        _session = session;
+        _updateModelAccessor = updateModelAccessor;
+    }
+
+    public override async Task UpdatedAsync(UpdateContentContext context, ProductPart instance)
+    {
+        var products = (await _session
+            .QueryContentItem(PublicationStatus.Published)
+            .ListAsync()).AsList();
+
+        var isNotUnique = products.Any(sku => sku.As<ProductPart>()?.Sku == instance.Sku);
+
+        if (isNotUnique)
+        {
+            _updateModelAccessor.ModelUpdater.ModelState.AddModelError(nameof(instance.Sku), "SKU must be unique.");
+        }
+    }
+}
