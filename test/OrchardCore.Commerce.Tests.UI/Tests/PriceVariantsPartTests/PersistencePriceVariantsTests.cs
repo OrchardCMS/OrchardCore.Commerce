102740c1fea05c1105587e2e0669f090a84f9b3a
Roland Márkus <roland.markus@lombiq.com>
2022-08-05 15:26:47 +0200

    Adding test + sample type.

---
+++ test/OrchardCore.Commerce.Tests.UI/Tests/PriceVariantsPartTests/PersistencePriceVariantsTests.cs
@@ -0,0 +1,22 @@
+﻿using Lombiq.Tests.UI.Extensions;
+using Lombiq.Tests.UI.Services;
+using Xunit.Abstractions;
+
+namespace OrchardCore.Commerce.Tests.UI.Tests.PriceVariantsPartTests;
+
+public class PersistencePriceVariantsTests : UITestBase
+{
+    public PersistencePriceVariantsTests(ITestOutputHelper testOutputHelper)
+        : base(testOutputHelper)
+    {
+    }
+
+    public Task CreatingNewPriceVariantShouldPersist(Browser browser) =>
+        ExecuteTestAfterSetupAsync(
+            async context =>
+            {
+                await context.SignInDirectlyAsync();
+                await context.CreateNewContentItemAsync("asd");
+            },
+            browser);
+}
