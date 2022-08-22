3e0ce0d1f7db4c7e974ec31cce88ecf6fbee0ae5
Szabolcs Deme <szabolcs.deme@lombiq.com>
2022-08-22 20:20:19 +0200

    Refactorings

--- src/Modules/OrchardCore.Commerce/Controllers/PaymentController.cs
+++ src/Modules/OrchardCore.Commerce/Controllers/PaymentController.cs
@@ -14,28 +14,23 @@ public class PaymentController : Controller
     private readonly ICardPaymentService _cardPaymentService;
     private readonly IStringLocalizer T;
     private readonly IAuthorizationService _authorizationService;
-    private readonly IHttpContextAccessor _hca;
 
     public PaymentController(
         ICardPaymentService cardPaymentService,
         IStringLocalizer<PaymentController> stringLocalizer,
-        IAuthorizationService authorizationService,
-        IHttpContextAccessor hca)
+        IAuthorizationService authorizationService)
     {
         _cardPaymentService = cardPaymentService;
         _authorizationService = authorizationService;
-        _hca = hca;
         T = stringLocalizer;
     }
 
     [Route("checkout")]
     public async Task<IActionResult> Index()
     {
-        var user = _hca.HttpContext?.User;
-
-        if (user != null && !await _authorizationService.AuthorizeAsync(user, Permissions.Checkout))
+        if (User != null && !await _authorizationService.AuthorizeAsync(User, Permissions.Checkout))
         {
-            return user.Identity.IsAuthenticated ? Forbid() : LocalRedirect("~/Login?ReturnUrl=~/checkout");
+            return User.Identity.IsAuthenticated ? Forbid() : LocalRedirect("~/Login?ReturnUrl=~/checkout");
         }
 
         return View();
