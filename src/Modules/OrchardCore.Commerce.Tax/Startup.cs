816be5a1705ed5c3ab59834dd2003e644bb4342b
Dávid El-Saig <david.el-saig@lombiq.com>
2022-09-05 16:56:20 +0200

    Add OrchardCore.Commerce.Tax module.

---
+++ src/Modules/OrchardCore.Commerce.Tax/Startup.cs
@@ -0,0 +1,29 @@
+using System;
+using Fluid;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Modules;
+
+namespace OrchardCore.Commerce.Tax
+{
+    public class Startup : StartupBase
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
+        {
+            routes.MapAreaControllerRoute(
+                name: "Home",
+                areaName: "OrchardCore.Commerce.Tax",
+                pattern: "Home/Index",
+                defaults: new
+                {
+                    controller = "Home", action = "Index"
+                }
+            );
+        }
+    }
+}
