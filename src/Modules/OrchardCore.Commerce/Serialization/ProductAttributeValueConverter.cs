bb14c8c62843f30c463efff557745caccc806ef9
Szabolcs Deme <80963259+DemeSzabolcs@users.noreply.github.com>
2022-09-11 16:58:00 +0200

    OCC-28: Order part breaks when it contains products with attributes (#174)
    * Adding ProductAttributeValueConverter to fix the bug
    
    * Using extension to simplify the converter
    
    * Refactoring converter
    
    * Styling attributes on the frontend
    
    * Update src/Modules/OrchardCore.Commerce/Serialization/ProductAttributeValueConverter.cs
    
    Co-authored-by: Dávid El-Saig <daud.ici@gmail.com>
    
    * Update src/Modules/OrchardCore.Commerce/Serialization/ProductAttributeValueConverter.cs
    
    Co-authored-by: Dávid El-Saig <daud.ici@gmail.com>
    
    * Checking if productAttributeValue.UntypedValue is IEnumerable
    
    * DRY
    
    Co-authored-by: Dávid El-Saig <daud.ici@gmail.com>

---
+++ src/Modules/OrchardCore.Commerce/Serialization/ProductAttributeValueConverter.cs
@@ -0,0 +1,70 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using OrchardCore.Commerce.Abstractions;
+using OrchardCore.Commerce.ProductAttributeValues;
+using System;
+using System.Collections.Generic;
+
+namespace OrchardCore.Commerce.Serialization;
+
+internal class ProductAttributeValueConverter : JsonConverter<IProductAttributeValue>
+{
+    private const string Type = "type";
+    private const string Value = "value";
+    private const string AttributeName = "attributeName";
+
+    public override IProductAttributeValue ReadJson(
+        JsonReader reader,
+        Type objectType,
+        IProductAttributeValue existingValue,
+        bool hasExistingValue,
+        JsonSerializer serializer)
+    {
+        var attribute = (JObject)JToken.Load(reader);
+
+        var attributeName = attribute.Get<string>(AttributeName);
+        var typeName = attribute.Get<string>(Type);
+
+        return typeName switch
+        {
+            nameof(TextProductAttributeValue) => new TextProductAttributeValue(attributeName, attribute.Get<IEnumerable<string>>(Value)),
+            nameof(BooleanProductAttributeValue) => new BooleanProductAttributeValue(attributeName, attribute.Get<bool>(Value)),
+            nameof(NumericProductAttributeValue) => new NumericProductAttributeValue(attributeName, attribute.Get<decimal>(Value)),
+            _ => throw new InvalidOperationException($"Unknown or unsupported type \"{typeName}\"."),
+        };
+    }
+
+    public override void WriteJson(JsonWriter writer, IProductAttributeValue productAttributeValue, JsonSerializer serializer)
+    {
+        if (productAttributeValue is null) return;
+
+        writer.WriteStartObject();
+        writer.WritePropertyName(Type);
+        writer.WriteValue(productAttributeValue.GetType().Name);
+
+        var untypedValue = productAttributeValue.UntypedValue;
+
+        writer.WritePropertyName(Value);
+
+        if (untypedValue is IEnumerable<object> values)
+        {
+            writer.WriteStartArray();
+
+            foreach (var value in values)
+            {
+                writer.WriteValue(value);
+            }
+
+            writer.WriteEndArray();
+        }
+        else
+        {
+            writer.WriteValue(untypedValue);
+        }
+
+        writer.WritePropertyName(AttributeName);
+        writer.WriteValue(productAttributeValue.AttributeName);
+
+        writer.WriteEndObject();
+    }
+}
