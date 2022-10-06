5974830a7e49f9e94fa81b0bb3171819fdf907f8
Gábor Pór <gabor.por@lombiq.com>
2022-10-06 11:39:47 +0200

    Improving documentation

--- src/Modules/OrchardCore.Commerce/Abstractions/IProductService.cs
+++ src/Modules/OrchardCore.Commerce/Abstractions/IProductService.cs
@@ -17,12 +17,12 @@ public interface IProductService
     Task<IEnumerable<ProductPart>> GetProductsAsync(IEnumerable<string> skus);
 
     /// <summary>
-    /// Returns the key the variant is identified by.
+    /// Returns the key the product variant is identified by.
     /// </summary>
     string GetVariantKey(string sku);
 
     /// <summary>
-    /// Returns the exact variant, as well as its identifying key, associated with the provided SKU.
+    /// Returns the exact variant of a product, as well as its identifying key, associated with the provided SKU.
     /// </summary>
     Task<(PriceVariantsPart Part, string VariantKey)> GetExactVariantAsync(string sku);
 }
