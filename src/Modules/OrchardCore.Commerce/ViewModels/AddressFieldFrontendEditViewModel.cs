0c22f0c304b8ddfa8898a1ad1f2fb4757bff4f22
Szabolcs Deme <szabolcs.deme@lombiq.com>
2022-10-07 18:01:16 +0200

    Creating a shape for AddressField Editor

---
+++ src/Modules/OrchardCore.Commerce/ViewModels/AddressFieldFrontendEditViewModel.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OrchardCore.Commerce.Fields;
+using OrchardCore.DisplayManagement.Views;
+using System.Collections.Generic;
+
+namespace OrchardCore.Commerce.ViewModels;
+
+public class AddressFieldFrontendEditViewModel : ShapeViewModel
+{
+    public AddressField AddressField { get; set; }
+    public string CityName { get; set; }
+
+    [BindNever]
+    public IEnumerable<SelectListItem> Regions { get; set; }
+
+    [BindNever]
+    // We don't want to mess with filling up this every time also, it's just a view-model so it's safe.
+#pragma warning disable CA2227 // Collection properties should be read only
+    public IDictionary<string, IDictionary<string, string>> Provinces { get; set; } =
+#pragma warning restore CA2227 // Collection properties should be read only
+        new Dictionary<string, IDictionary<string, string>>();
+
+    public AddressFieldFrontendEditViewModel() => Metadata.Type = "AddressFieldFrontendEdit";
+}
