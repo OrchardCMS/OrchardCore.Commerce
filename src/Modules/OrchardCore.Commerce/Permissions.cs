cd0d2d1777d8279f31b503e81b6de13a8074a2f2
Krisztián Németh <krisztian.nemeth@lombiq.com>
2022-10-25 12:10:44 +0200

    Adding productpart to order line viewmodel

--- src/Modules/OrchardCore.Commerce/Permissions.cs
+++ src/Modules/OrchardCore.Commerce/Permissions.cs
@@ -9,10 +9,18 @@ public class Permissions : IPermissionProvider
     public static readonly Permission ManageCurrencySettings = new("ManageCurrencySettings", "Manage Currency Settings");
     public static readonly Permission ManageStripeApiSettings = new("ManageStripeApiSettings", "Manage Stripe API Settings");
     public static readonly Permission ManageRegionSettings = new("ManageRegionSettings", "Manage Region Settings");
+    public static readonly Permission ManageOrderSettings = new("ManageOrderSettings", "Manage Order Settings");
     public static readonly Permission Checkout = new("Checkout", "Ability to checkout");
 
     public Task<IEnumerable<Permission>> GetPermissionsAsync() =>
-        Task.FromResult<IEnumerable<Permission>>(new[] { ManageCurrencySettings, ManageStripeApiSettings, Checkout, ManageRegionSettings });
+        Task.FromResult<IEnumerable<Permission>>(new[]
+        {
+            ManageCurrencySettings,
+            ManageStripeApiSettings,
+            Checkout,
+            ManageRegionSettings,
+            ManageOrderSettings,
+        });
 
     public IEnumerable<PermissionStereotype> GetDefaultStereotypes() =>
         new[]
@@ -20,7 +28,14 @@ public class Permissions : IPermissionProvider
             new PermissionStereotype
             {
                 Name = "Administrator",
-                Permissions = new[] { ManageCurrencySettings, ManageStripeApiSettings, Checkout, ManageRegionSettings },
+                Permissions = new[]
+                {
+                    ManageCurrencySettings,
+                    ManageStripeApiSettings,
+                    Checkout,
+                    ManageRegionSettings,
+                    ManageOrderSettings,
+                },
             },
         };
 }
