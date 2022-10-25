4f6b86bfb28d9696db99bae13e4cd112ec57b4fa
Krisztián Németh <krisztian.nemeth@lombiq.com>
2022-10-25 16:06:15 +0200

    Exposing new viewmodels and adding new filter

---
+++ src/Modules/OrchardCore.Commerce/Liquid/AddressFieldEditorViewModelConverterFilter.cs
@@ -0,0 +1,31 @@
+using Fluid;
+using Fluid.Values;
+using OrchardCore.Commerce.Fields;
+using OrchardCore.Commerce.ViewModels;
+using OrchardCore.Liquid;
+using System.Threading.Tasks;
+
+namespace OrchardCore.Commerce.Liquid;
+
+public class AddressFieldEditorViewModelConverterFilter : ILiquidFilter
+{
+    public ValueTask<FluidValue> ProcessAsync(FluidValue input, FilterArguments arguments, LiquidTemplateContext context)
+    {
+        if (input?.ToObjectValue() is not CheckoutViewModel checkoutViewModel) return new ValueTask<FluidValue>(input);
+
+        var cityName = arguments["city_name"].Or(arguments.At(0)).ToStringValue();
+
+        if (arguments["address_field"].Or(arguments.At(0)).ToObjectValue() is not AddressField addressField ||
+            string.IsNullOrEmpty(cityName)) return new ValueTask<FluidValue>(input);
+
+        var viewModel = new AddressFieldEditorViewModel()
+        {
+            AddressField = addressField,
+            CityName = cityName,
+            Regions = checkoutViewModel.Regions,
+            Provinces = checkoutViewModel.Provinces,
+        };
+
+        return new ValueTask<FluidValue>(new ObjectValue(viewModel));
+    }
+}
