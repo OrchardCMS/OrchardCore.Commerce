4942162c4abb804c234b34b205fe36a746545f0b
Szabolcs Deme <szabolcs.deme@lombiq.com>
2022-11-24 19:20:11 +0100

    Refactoring script

--- src/Modules/OrchardCore.Commerce/Assets/Scripts/discount.js
+++ src/Modules/OrchardCore.Commerce/Assets/Scripts/discount.js
@@ -1,13 +1,7 @@
-const grossPrice = document.querySelector('span.tax-part-gross-price-value');
-const defaultPrice = document.querySelector('span.price-part-price-field-value');
+const prices = Array.from(document.querySelectorAll('.tax-part-gross-price-value, .price-part-price-field-value'));
 
 function strikeOutPrice(price) {
     price.innerHTML = "<del class=\"text-danger\">" + price.textContent + "</del>";
 }
 
-if (grossPrice) {
-    strikeOutPrice(grossPrice);
-}
-else if (defaultPrice) {
-    strikeOutPrice(defaultPrice);
-}
+prices.forEach(price => strikeOutPrice(price));
