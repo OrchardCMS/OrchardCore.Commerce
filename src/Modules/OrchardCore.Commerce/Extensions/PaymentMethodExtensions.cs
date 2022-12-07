6a15c54d783fa54d1d97f6804b27cf0df5a7124d
Krisztián Németh <krisztian.nemeth@lombiq.com>
2022-12-07 13:08:54 +0100

    Adding payment method type formatter extension

---
+++ src/Modules/OrchardCore.Commerce/Extensions/PaymentMethodExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Stripe;
+
+namespace OrchardCore.Commerce.Extensions;
+
+public static class PaymentMethodExtensions
+{
+    public static string GetFormattedPaymentType(this PaymentMethod paymentMethod) =>
+        paymentMethod.Type switch
+        {
+            "acss_debit" => "Pre-authorized debit payments",
+            "affirm" => "Affirm",
+            "afterpay_clearpay" => "Afterpay / Clearpay",
+            "alipay" => "Alipay",
+            "au_becs_debit" => "BECS Direct Debit ",
+            "bacs_debit" => "Bacs Direct Debit",
+            "bancontact" => "Bancontact",
+            "blik" => "BLIK",
+            "boleto" => "Boleto",
+            "card" => "Card",
+            "card_present" => "Stripe Terminal",
+            "customer_balance" => "Cash balance",
+            "eps" => "EPS",
+            "fpx" => "FPX",
+            "giropay" => "giropay",
+            "grabpay" => "GrabPay",
+            "ideal" => "iDEAL",
+            "interac_present" => "Interac",
+            "klarna" => "Klarna",
+            "konbini" => "Konbini",
+            "link" => "Link",
+            "oxxo" => "OXXO",
+            "p24" => "Przelewy24",
+            "paynow" => "PayNow",
+            "pix" => "Pix",
+            "promptpay" => "PromptPay",
+            "sepa_debit" => "SEPA Direct Debit",
+            "sofort" => "Sofort",
+            "us_bank_account" => "ACH Direct Debit",
+            "wechat_pay" => "WeChat Pay",
+            _ => paymentMethod.Type,
+        };
+}
