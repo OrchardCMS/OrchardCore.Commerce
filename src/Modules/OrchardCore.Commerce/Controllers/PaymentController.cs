925ecf0cbf35a506ff27b070a5fdf2ed05ec3398
Krisztián Németh <krisztian.nemeth@lombiq.com>
2022-12-11 18:09:22 +0100

    Code styling

--- src/Modules/OrchardCore.Commerce/Controllers/PaymentController.cs
+++ src/Modules/OrchardCore.Commerce/Controllers/PaymentController.cs
@@ -150,9 +150,8 @@ public class PaymentController : Controller
             UserEmail = email,
             CheckoutShapes = checkoutShapes,
             PaymentIntentClientSecret = initPaymentIntent?.ClientSecret,
-            EnableInputs =
-                initPaymentIntent?.Status is not PaymentIntentStatuses.Succeeded
-                    and not PaymentIntentStatuses.Processing,
+            EnableInputs = initPaymentIntent?.Status is not PaymentIntentStatuses.Succeeded
+                and not PaymentIntentStatuses.Processing,
         };
 
         foreach (dynamic shape in checkoutShapes) shape.ViewModel = checkoutViewModel;
