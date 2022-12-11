fb7818f17e1713796ea8475d0947e816f8ead66d
Krisztián Németh <krisztian.nemeth@lombiq.com>
2022-12-11 16:47:22 +0100

    Renaming variable to better represent role

--- src/Modules/OrchardCore.Commerce/Services/PaymentIntentPersistence.cs
+++ src/Modules/OrchardCore.Commerce/Services/PaymentIntentPersistence.cs
@@ -8,14 +8,14 @@ namespace OrchardCore.Commerce.Services;
 
 public class PaymentIntentPersistence : IPaymentIntentPersistence
 {
-    private const string PaymentIntentPrefix = "OrchardCore:Commerce:PaymentIntent:";
+    private const string PaymentIntentKey = "OrchardCore:Commerce:PaymentIntent";
 
     private readonly IHttpContextAccessor _httpContextAccessor;
     private ISession Session => _httpContextAccessor.HttpContext?.Session;
 
     public PaymentIntentPersistence(IHttpContextAccessor httpContextAccessor) => _httpContextAccessor = httpContextAccessor;
 
-    public string Retrieve() => Session.GetString(PaymentIntentPrefix);
+    public string Retrieve() => Session.GetString(PaymentIntentKey);
 
-    public void Store(string paymentIntentId) => Session.SetString(PaymentIntentPrefix, paymentIntentId);
+    public void Store(string paymentIntentId) => Session.SetString(PaymentIntentKey, paymentIntentId);
 }
