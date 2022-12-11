e93c4075a000780c6524b8587b2f1bf6b2828c19
Krisztián Németh <krisztian.nemeth@lombiq.com>
2022-12-11 16:22:43 +0100

    Adding const strings for statuses

---
+++ src/Modules/OrchardCore.Commerce/Constants/PaymentIntentStatuses.cs
@@ -0,0 +1,12 @@
+﻿namespace OrchardCore.Commerce.Constants;
+
+public static class PaymentIntentStatuses
+{
+    public const string RequiresPaymentMethod = "requires_payment_method";
+    public const string RequiresConfirmation = "requires_confirmation";
+    public const string RequiresAction = "requires_action";
+    public const string Processing = "processing";
+    public const string RequiresCapture = "requires_capture";
+    public const string Canceled = "canceled";
+    public const string Succeeded = "succeeded";
+}
