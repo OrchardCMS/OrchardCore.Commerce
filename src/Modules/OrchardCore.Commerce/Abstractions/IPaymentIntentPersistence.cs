a03b9d378d373640afdbc4c4e6e6b315ac3bb294
Krisztián Németh <krisztian.nemeth@lombiq.com>
2022-12-11 16:29:25 +0100

    Changing docs

--- src/Modules/OrchardCore.Commerce/Abstractions/IPaymentIntentPersistence.cs
+++ src/Modules/OrchardCore.Commerce/Abstractions/IPaymentIntentPersistence.cs
@@ -1,17 +1,17 @@
 namespace OrchardCore.Commerce.Abstractions;
 
 /// <summary>
-/// Service that provides a way to retain shopping cart information.
+/// Service that provides a way to retain the current payment intent Id from the current session.
 /// </summary>
 public interface IPaymentIntentPersistence
 {
     /// <summary>
-    /// Returns the payment intent id stored in current session.
+    /// Returns the payment intent Id stored in the current session.
     /// </summary>
     string Retrieve();
 
     /// <summary>
-    /// Saves a payment intent Id.
+    /// Saves a payment intent Id to the session.
     /// </summary>
     void Store(string paymentIntentId);
 }
