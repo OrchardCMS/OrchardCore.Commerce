29b1f80b85b488abee8451320c8eff435a5a2064
Gábor Pór <gabor.por@lombiq.com>
2022-12-27 13:42:42 +0100

    Adding new SortableUpdateProvider extension method to return provider of specific type

--- src/Modules/OrchardCore.Commerce/Extensions/ProductInventoryProviderExtensions.cs
+++ src/Modules/OrchardCore.Commerce/Extensions/ProductInventoryProviderExtensions.cs
@@ -1,9 +1,16 @@
+using OrchardCore.Commerce.Models;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace OrchardCore.Commerce.Abstractions;
 
 public static class ProductInventoryProviderExtensions
 {
-    public static async Task<bool> IsAvailableAsync(this IProductInventoryProvider provider, string sku) =>
-        await provider.QueryInventoryAsync(sku) > 0;
+    public static async Task<bool> IsAvailableAsync(
+        this IEnumerable<IProductInventoryProvider> providers, string sku, IList<ShoppingCartItem> model)
+    {
+        var provider = await providers.GetFirstApplicableProviderAsync<IList<ShoppingCartItem>, IProductInventoryProvider>(model);
+
+        return await provider.QueryInventoryAsync(sku) > 0;
+    }
 }
