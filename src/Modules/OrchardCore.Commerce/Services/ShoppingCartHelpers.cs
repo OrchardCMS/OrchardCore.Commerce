134a1fcca346cbe1ece5f7e76265f7ac93c5c9cb
Dávid El-Saig <david.el-saig@lombiq.com>
2023-01-09 12:53:09 +0100

    Unusing.

--- src/Modules/OrchardCore.Commerce/Services/ShoppingCartHelpers.cs
+++ src/Modules/OrchardCore.Commerce/Services/ShoppingCartHelpers.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Localization;
 using OrchardCore.Commerce.Abstractions;
 using OrchardCore.Commerce.AddressDataType;
@@ -7,7 +6,6 @@ using OrchardCore.Commerce.Extensions;
 using OrchardCore.Commerce.Models;
 using OrchardCore.Commerce.MoneyDataType;
 using OrchardCore.Commerce.ViewModels;
-using OrchardCore.Users;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
