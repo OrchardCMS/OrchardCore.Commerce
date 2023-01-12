1fa5a6dfff859e7652422db3983c5a3c63411474
Dávid El-Saig <david.el-saig@lombiq.com>
2023-01-12 02:26:45 +0100

    Remove Source Address fields.

--- src/Modules/OrchardCore.Commerce.Tax/Models/TaxRateSettings.cs
+++ src/Modules/OrchardCore.Commerce.Tax/Models/TaxRateSettings.cs
@@ -4,24 +4,10 @@ namespace OrchardCore.Commerce.Tax.Models;
 
 public class TaxRateSettings
 {
-    public string SourceStreetAddress1 { get; set; }
-    public string SourceStreetAddress2 { get; set; }
-    public string SourceCity { get; set; }
-    public string SourceProvince { get; set; }
-    public string SourcePostalCode { get; set; }
-    public string SourceRegion { get; set; }
-
     public IList<TaxRateSetting> Rates { get; } = new List<TaxRateSetting>();
 
     public void CopyFrom(TaxRateSettings other)
     {
-        SourceStreetAddress1 = other.SourceStreetAddress1;
-        SourceStreetAddress2 = other.SourceStreetAddress2;
-        SourceCity = other.SourceCity;
-        SourceProvince = other.SourceProvince;
-        SourcePostalCode = other.SourcePostalCode;
-        SourceRegion = other.SourceRegion;
-
         Rates.Clear();
         Rates.AddRange(other.Rates);
     }
