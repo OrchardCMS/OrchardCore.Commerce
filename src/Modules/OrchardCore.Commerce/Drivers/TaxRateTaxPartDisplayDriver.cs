d3d2c1ce8655215bc819023ff7b1eb302adb5179
Dávid El-Saig <david.el-saig@lombiq.com>
2023-01-28 18:06:37 +0100

    fix bug: two gross prices show up when logged in

--- src/Modules/OrchardCore.Commerce/Drivers/TaxRateTaxPartDisplayDriver.cs
+++ src/Modules/OrchardCore.Commerce/Drivers/TaxRateTaxPartDisplayDriver.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Http;
+using OrchardCore.Commerce.Abstractions;
 using OrchardCore.Commerce.Models;
 using OrchardCore.Commerce.Services;
 using OrchardCore.Commerce.Tax.Models;
@@ -7,6 +8,7 @@ using OrchardCore.ContentManagement;
 using OrchardCore.ContentManagement.Display.ContentDisplay;
 using OrchardCore.ContentManagement.Display.Models;
 using OrchardCore.DisplayManagement.Views;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace OrchardCore.Commerce.Drivers;
@@ -14,34 +16,40 @@ namespace OrchardCore.Commerce.Drivers;
 public class TaxRateTaxPartDisplayDriver : ContentPartDisplayDriver<TaxPart>
 {
     private readonly IHttpContextAccessor _hca;
-    private readonly TaxRateTaxProvider _taxRateTaxProvider;
+    private readonly IEnumerable<ITaxProvider> _taxProviders;
 
     public TaxRateTaxPartDisplayDriver(
         IHttpContextAccessor hca,
-        TaxRateTaxProvider taxRateTaxProvider)
+        IEnumerable<ITaxProvider> taxProviders)
     {
         _hca = hca;
-        _taxRateTaxProvider = taxRateTaxProvider;
+        _taxProviders = taxProviders;
     }
 
     public override async Task<IDisplayResult> DisplayAsync(TaxPart part, BuildPartDisplayContext context)
     {
         if (part.As<ProductPart>() is not { } product ||
-            part.As<PricePart>()?.Price is not { } netUnitPrice)
+            part.As<PricePart>()?.Price is not { } netUnitPrice ||
+            _hca.HttpContext is not { } httpContext)
         {
             return null;
         }
 
-        var userAddresses = await _hca.HttpContext.GetUserAddressAsync();
+        var userAddresses = await httpContext.GetUserAddressAsync();
         var model = PromotionAndTaxProviderContext.SingleProduct(
             product,
             netUnitPrice,
             userAddresses?.ShippingAddress.Address,
             userAddresses?.BillingAddress.Address);
-        if (!await _taxRateTaxProvider.IsApplicableAsync(model)) return null;
 
-        model = await _taxRateTaxProvider.UpdateAsync(model);
+        if (await _taxProviders.GetFirstApplicableProviderAsync(model) is not TaxRateTaxProvider taxRateTaxProvider)
+        {
+            return null;
+        }
 
+        httpContext.Items[nameof(TaxRateTaxPartDisplayDriver)] = true;
+
+        model = await taxRateTaxProvider.UpdateAsync(model);
         return Initialize<TaxRateViewModel>("TaxPart_TaxRate_GrossPrice", viewModel =>
             {
                 viewModel.Context = model;
