b17c3d01769faa0a9d34bac78b2a57ccd59a7d08
Krisztián Németh <krisztian.nemeth@lombiq.com>
2023-03-15 16:33:23 +0100

    Not needed

--- src/OrchardCore.Commerce.Web/.stylelintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    // The following path may have to be adjusted to your directory structure.
-    extends: './node_modules/nodejs-extensions/config/.stylelintrc.lombiq-base.js',
-
-    // Add custom rules and overrides here.
-    rules: {
-    },
-};
