8a6d80f014f3ee5309ef611825da5a9cb460ea82
Krisztián Németh <krisztian.nemeth@lombiq.com>
2023-04-06 17:02:00 +0200

    Adding necessities to module

---
+++ test/OrchardCore.Commerce.Tests.UI.Shortcuts/Manifest.cs
@@ -0,0 +1,25 @@
+using OrchardCore.Modules.Manifest;
+using static OrchardCore.Commerce.Tests.UI.Shortcuts.ShortcutsFeatureIds;
+
+[assembly: Module(
+    Name = "Shortcuts - Orchard Core Commerce",
+    Author = "The Orchard Team",
+    Website = "https://github.com/OrchardCMS/OrchardCore.Commerce",
+    Version = "0.0.1"
+)]
+
+[assembly: Feature(
+    Id = Default,
+    Name = "Lombiq UI Testing Toolbox - Shortcuts",
+    Category = "Development",
+    Description = "WARNING: Only enable this feature in the UI testing environment. Provides shortcuts for Orchard " +
+        "Core Commerce operations that UI tests might want to do or check.",
+    Dependencies = new[]
+    {
+        "OrchardCore.ContentManagement",
+        "OrchardCore.ContentTypes",
+        "OrchardCore.DisplayManagement",
+        "OrchardCore.Users",
+        "OrchardCore.Tenants",
+    }
+)]
