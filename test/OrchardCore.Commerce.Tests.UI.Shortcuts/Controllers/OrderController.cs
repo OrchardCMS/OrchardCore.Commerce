c9bc7ba9e4c7aee1998997a5da9cc226f432c7f4
Krisztián Németh <krisztian.nemeth@lombiq.com>
2023-04-12 12:16:35 +0200

    Setting addresses

--- test/OrchardCore.Commerce.Tests.UI.Shortcuts/Controllers/OrderController.cs
+++ test/OrchardCore.Commerce.Tests.UI.Shortcuts/Controllers/OrderController.cs
@@ -2,8 +2,10 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OrchardCore.Commerce.Abstractions;
+using OrchardCore.Commerce.AddressDataType;
 using OrchardCore.Commerce.Constants;
 using OrchardCore.Commerce.Controllers;
+using OrchardCore.Commerce.Fields;
 using OrchardCore.Commerce.Models;
 using OrchardCore.ContentFields.Fields;
 using OrchardCore.ContentManagement;
@@ -11,7 +13,7 @@ using OrchardCore.ContentManagement.Utilities;
 using OrchardCore.Modules;
 using OrchardCore.Mvc.Core.Utilities;
 using System;
-using System.Linq;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using static OrchardCore.Commerce.Constants.ContentTypes;
 
@@ -22,30 +24,32 @@ public class OrderController : Controller
 {
     private readonly Lazy<ICheckoutService> _checkoutService;
     private readonly Lazy<IShoppingCartPersistence> _shoppingCartPersistence;
-    private readonly IClock _clock;
     private readonly Lazy<IContentManager> _contentManager;
+    private readonly Lazy<IStripePaymentService> _stripePaymentService;
 
     public OrderController(
         Lazy<ICheckoutService> checkoutService,
         Lazy<IShoppingCartPersistence> shoppingCartPersistence,
-        IClock clock,
-        Lazy<IContentManager> contentManager)
+        Lazy<IContentManager> contentManager,
+        Lazy<IStripePaymentService> stripePaymentService)
     {
         _checkoutService = checkoutService;
         _shoppingCartPersistence = shoppingCartPersistence;
-        _clock = clock;
         _contentManager = contentManager;
+        _stripePaymentService = stripePaymentService;
     }
 
     [AllowAnonymous]
-    public async Task<IActionResult> CreateOrderWithSuccessfulPayment()
+    public async Task<IActionResult> CreateOrderWithSuccessfulPayment(long dateTimeTicks)
     {
-        var testTime = _clock.UtcNow;
+        var testTime = new DateTime(dateTimeTicks);
 
         var shoppingCart = await _shoppingCartPersistence.Value.RetrieveAsync();
         var checkoutViewModel = await _checkoutService.Value.CreateCheckoutViewModelAsync(shoppingCart.Id);
         var order = await _contentManager.Value.NewAsync(Order);
+        var orderLineItemList = await _stripePaymentService.Value.CreateOrderLineItemListAsync(shoppingCart);
         order.Apply(checkoutViewModel.OrderPart);
+
         order.Alter<OrderPart>(orderPart =>
         {
             var payment = new Payment
@@ -60,6 +64,40 @@ public class OrderController : Controller
             orderPart.Charges.Clear();
             orderPart.Charges.Add(payment);
 
+            orderPart.LineItems.AddRange(orderLineItemList);
+
+            orderPart.BillingAddress = new AddressField
+            {
+                Address = new Address
+                {
+                    City = "TestCity",
+                    Company = "TestCompany",
+                    Department = "TestDepartment",
+                    Name = "TestName",
+                    Province = "TestProvince",
+                    Region = "TestRegion",
+                    PostalCode = "TestPostalCode",
+                    StreetAddress1 = "TestStreetAddress1",
+                    StreetAddress2 = "TestStreetAddress2",
+                },
+            };
+            orderPart.BillingAndShippingAddressesMatch.Value = true;
+            orderPart.ShippingAddress = new AddressField
+            {
+                Address = new Address
+                {
+                    City = "TestCity",
+                    Company = "TestCompany",
+                    Department = "TestDepartment",
+                    Name = "TestName",
+                    Province = "TestProvince",
+                    Region = "TestRegion",
+                    PostalCode = "TestPostalCode",
+                    StreetAddress1 = "TestStreetAddress1",
+                    StreetAddress2 = "TestStreetAddress2",
+                },
+            };
+
             orderPart.Status = new TextField { ContentItem = order, Text = OrderStatuses.Ordered.HtmlClassify() };
         });
 
@@ -70,10 +108,6 @@ public class OrderController : Controller
         return RedirectToAction(
             nameof(PaymentController.Success),
             typeof(PaymentController).ControllerName(),
-            new
-            {
-                area = "OrchardCore.Commerce",
-                orderId = order.ContentItemId,
-            });
+            new { area = "OrchardCore.Commerce", orderId = order.ContentItemId, });
     }
 }
