c2247d1618e66130eb0d33548530b388834871e9
Krisztián Németh <krisztian.nemeth@lombiq.com>
2023-04-19 12:44:00 +0200

    Improving docs

--- src/Modules/OrchardCore.Commerce/Abstractions/IPaymentService.cs
+++ src/Modules/OrchardCore.Commerce/Abstractions/IPaymentService.cs
@@ -1,4 +1,5 @@
-﻿using OrchardCore.Commerce.Models;
+﻿using OrchardCore.Commerce.Controllers;
+using OrchardCore.Commerce.Models;
 using OrchardCore.Commerce.ViewModels;
 using OrchardCore.ContentManagement;
 using System;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 namespace OrchardCore.Commerce.Abstractions;
 
 /// <summary>
-/// Services related to payment and PaymentController.
+/// Services related to payment and <see cref="PaymentController"/>.
 /// </summary>
 public interface IPaymentService
 {
@@ -19,7 +20,7 @@ public interface IPaymentService
         Action<OrderPart> updateOrderPart = null);
 
     /// <summary>
-    /// When the order is payed this logic should be run to finalize <paramref name="order"/>.
+    /// When the order is payed this logic should be run to set <paramref name="order"/> properties that represents its state.
     /// </summary>
     Task FinalModificationOfOrderAsync(ContentItem order);
 }
