9a22dd6c2b4682ab73dbc57957b5c9ccdd9a091d
Gábor Pór <gabor.por@lombiq.com>
2023-07-14 15:42:49 +0200

    Handling out of stock message

--- src/Modules/OrchardCore.Commerce.Inventory/Models/InventoryPart.cs
+++ src/Modules/OrchardCore.Commerce.Inventory/Models/InventoryPart.cs
@@ -8,6 +8,7 @@ public class InventoryPart : ContentPart
 {
     public BooleanField AllowsBackOrder { get; set; } = new();
     public BooleanField IgnoreInventory { get; set; } = new();
+
     public IDictionary<string, int> Inventory { get; } = new Dictionary<string, int>();
 
     public NumericField MaximumOrderQuantity { get; set; } = new(); // u too
