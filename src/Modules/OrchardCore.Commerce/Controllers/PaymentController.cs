718d8d131b0d7bf27fc1615f9ad32d827c95deae
Gábor Pór <gabor.por@lombiq.com>
2023-07-22 19:19:36 +0200

    Authorizing in PaymentController

--- src/Modules/OrchardCore.Commerce/Controllers/PaymentController.cs
+++ src/Modules/OrchardCore.Commerce/Controllers/PaymentController.cs
@@ -155,6 +155,12 @@ public class PaymentController : Controller
     {
         if (await _contentManager.GetAsync(orderId) is not { } order) return NotFound();
 
+        // Regular users should only see their own orders.
+        if (!await _authorizationService.AuthorizeAsync(User, Permissions.ManageOrders) && order.Author != User.Identity.Name)
+        {
+            return NotFound();
+        }
+
         order.DisplayText = T["Success"].Value; // This is only for display, intentionally not saved.
         return View(order);
     }
