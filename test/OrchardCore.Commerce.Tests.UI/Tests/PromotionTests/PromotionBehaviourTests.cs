6ef3a24406566e39a07875219737700060c524a5
Gábor Pór <gabor.por@lombiq.com>
2023-08-02 15:48:21 +0200

    Adding UI test for discount behavior

--- test/OrchardCore.Commerce.Tests.UI/Tests/PromotionTests/PromotionBehaviourTests.cs
+++ test/OrchardCore.Commerce.Tests.UI/Tests/PromotionTests/PromotionBehaviourTests.cs
@@ -3,6 +3,7 @@ using Lombiq.Tests.UI.Attributes;
 using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Services;
 using OpenQA.Selenium;
+using Shouldly;
 using Xunit;
 using Xunit.Abstractions;
 using static OrchardCore.Commerce.Tests.UI.Constants.ContentItemIds;
@@ -33,6 +34,35 @@ public class PromotionBehaviourTests : UITestBase
                 await CheckDiscountPresenceAsync(hasNewPrice: false);
                 await context.SignInDirectlyAsync();
                 await CheckDiscountPresenceAsync(hasNewPrice: true);
+            },
+            browser);
+
+    [Theory, Chrome]
+    public Task DiscountShouldProperlyAppearOnDiscountedProduct(Browser browser) =>
+        ExecuteTestAfterSetupAsync(
+            async context =>
+            {
+                // Discount should appear on discounted product's page.
+                await context.GoToRelativeUrlAsync("/testdiscountedproduct");
+                context.Get(By.CssSelector(".price-part-price-field-value > del")).Text.ShouldBe("$5.00");
+                context.Get(By.CssSelector(".price-part-price-field-value > span")).Text.ShouldBe("$3.00");
+                await context.ClickReliablyOnSubmitAsync();
+
+                // Discount should appear in cart when discounted product is the only item in the cart.
+                context.Get(By.CssSelector(".shopping-cart-table-unit-price")).Text.ShouldBe("$3.00");
+
+                // Discount should appear in cart when discounted product is not the only item in the cart.
+                await context.GoToRelativeUrlAsync("/testproduct");
+                await context.ClickReliablyOnSubmitAsync();
+                context.Get(By.CssSelector(".shopping-cart-table-unit-price")).Text.ShouldBe("$3.00");
+
+                // Total should reflect discount as well.
+                context.Get(By.CssSelector(".shopping-cart-table-totals > div")).Text.ShouldBe("$8.00");
+
+                // Discount should still appear on discounted product's page when the cart contains a full-price item.
+                await context.GoToRelativeUrlAsync("/testdiscountedproduct");
+                context.Get(By.CssSelector(".price-part-price-field-value > del")).Text.ShouldBe("$5.00");
+                context.Get(By.CssSelector(".price-part-price-field-value > span")).Text.ShouldBe("$3.00");
             },
             browser);
 }
