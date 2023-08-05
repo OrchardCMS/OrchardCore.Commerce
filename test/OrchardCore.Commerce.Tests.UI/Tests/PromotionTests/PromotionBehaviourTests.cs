3eceb6deca309e59181078260c1a27d0e1fa5b7d
Gábor Pór <gabor.por@lombiq.com>
2023-08-05 11:45:32 +0200

    Adding constants to UI test

--- test/OrchardCore.Commerce.Tests.UI/Tests/PromotionTests/PromotionBehaviourTests.cs
+++ test/OrchardCore.Commerce.Tests.UI/Tests/PromotionTests/PromotionBehaviourTests.cs
@@ -12,6 +12,8 @@ namespace OrchardCore.Commerce.Tests.UI.Tests.PromotionTests;
 
 public class PromotionBehaviourTests : UITestBase
 {
+    public const string PriceFieldCssSelector = ".price-part-price-field-value";
+
     public PromotionBehaviourTests(ITestOutputHelper testOutputHelper)
         : base(testOutputHelper)
     {
@@ -26,7 +28,7 @@ public class PromotionBehaviourTests : UITestBase
                 {
                     await context.GoToContentItemByIdAsync(TestProduct);
                     context.Exists(By.ClassName("price-part-price-field-value"));
-                    context.CheckExistence(By.CssSelector(".price-part-price-field-value del"), hasNewPrice);
+                    context.CheckExistence(By.CssSelector($"{PriceFieldCssSelector} del"), hasNewPrice);
                 }
 
                 await context.ExecuteRecipeDirectlyAsync("OrchardCore.Commerce.Samples.GlobalDiscount");
@@ -42,27 +44,30 @@ public class PromotionBehaviourTests : UITestBase
         ExecuteTestAfterSetupAsync(
             async context =>
             {
+                const string FullPrice = "$5.00";
+                const string DiscountedPrice = "$3.00";
+
                 // Discount should appear on discounted product's page.
                 await context.GoToRelativeUrlAsync("/testdiscountedproduct");
-                context.Get(By.CssSelector(".price-part-price-field-value > del")).Text.ShouldBe("$5.00");
-                context.Get(By.CssSelector(".price-part-price-field-value > span")).Text.ShouldBe("$3.00");
+                context.Get(By.CssSelector($"{PriceFieldCssSelector} > del")).Text.ShouldBe(FullPrice);
+                context.Get(By.CssSelector($"{PriceFieldCssSelector} > span")).Text.ShouldBe(DiscountedPrice);
                 await context.ClickReliablyOnSubmitAsync();
 
                 // Discount should appear in cart when discounted product is the only item in the cart.
-                context.Get(By.CssSelector(".shopping-cart-table-unit-price")).Text.ShouldBe("$3.00");
+                context.Get(By.CssSelector(".shopping-cart-table-unit-price")).Text.ShouldBe(DiscountedPrice);
 
                 // Discount should appear in cart when discounted product is not the only item in the cart.
                 await context.GoToRelativeUrlAsync("/testproduct");
                 await context.ClickReliablyOnSubmitAsync();
-                context.Get(By.CssSelector(".shopping-cart-table-unit-price")).Text.ShouldBe("$3.00");
+                context.Get(By.CssSelector(".shopping-cart-table-unit-price")).Text.ShouldBe(DiscountedPrice);
 
                 // Total should reflect discount as well.
                 context.Get(By.CssSelector(".shopping-cart-table-totals > div")).Text.ShouldBe("$8.00");
 
                 // Discount should still appear on discounted product's page when the cart contains a full-price item.
                 await context.GoToRelativeUrlAsync("/testdiscountedproduct");
-                context.Get(By.CssSelector(".price-part-price-field-value > del")).Text.ShouldBe("$5.00");
-                context.Get(By.CssSelector(".price-part-price-field-value > span")).Text.ShouldBe("$3.00");
+                context.Get(By.CssSelector($"{PriceFieldCssSelector} > del")).Text.ShouldBe(FullPrice);
+                context.Get(By.CssSelector($"{PriceFieldCssSelector} > span")).Text.ShouldBe(DiscountedPrice);
             },
             browser);
 }
