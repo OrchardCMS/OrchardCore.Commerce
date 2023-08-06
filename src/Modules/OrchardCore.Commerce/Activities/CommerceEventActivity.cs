797fb56404700374e5f1213254761b2e1163223f
Dávid El-Saig <david.el-saig@lombiq.com>
2023-08-06 23:11:05 +0200

    Create base event.

---
+++ src/Modules/OrchardCore.Commerce/Activities/CommerceEventActivity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Localization;
+using OrchardCore.Workflows.Abstractions.Models;
+using OrchardCore.Workflows.Activities;
+using OrchardCore.Workflows.Models;
+using System.Collections.Generic;
+
+namespace OrchardCore.Commerce.Activities;
+
+public abstract class CommerceEventActivity : EventActivity
+{
+    protected readonly IStringLocalizer T;
+
+    public override string Name => GetType().Name;
+    public abstract override LocalizedString DisplayText { get; }
+    public override LocalizedString Category => T["Commerce"];
+
+    protected CommerceEventActivity(IStringLocalizer stringLocalizer) =>
+        T = stringLocalizer;
+
+    public override IEnumerable<Outcome> GetPossibleOutcomes(
+        WorkflowExecutionContext workflowContext,
+        ActivityContext activityContext) =>
+        new[] { new Outcome(T["Done"]) };
+
+    public override ActivityExecutionResult Resume(
+        WorkflowExecutionContext workflowContext,
+        ActivityContext activityContext) =>
+        Outcomes("Done");
+}
