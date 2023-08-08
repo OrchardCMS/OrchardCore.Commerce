00e00fe310df8287852519485a5c7acc994c35e4
Gábor Pór <gabor.por@lombiq.com>
2023-08-08 15:11:22 +0200

    Adding dropdown for currency

--- src/Modules/OrchardCore.Commerce/Drivers/OrderPartDisplayDriver.cs
+++ src/Modules/OrchardCore.Commerce/Drivers/OrderPartDisplayDriver.cs
@@ -61,11 +61,16 @@ public class OrderPartDisplayDriver : ContentPartDisplayDriver<OrderPart>
         var orderLineItems = new List<OrderLineItem>();
         foreach (var lineItem in viewModelLineItems)
         {
-            var productPart = await _productService.GetProductAsync(lineItem.ProductSku);
+            //var productPart = await _productService.GetProductAsync(lineItem.ProductSku);
+            // If the provided SKU does not belong to an existing product content item, it should not be added.
+            if (await _productService.GetProductAsync(lineItem.ProductSku.ToUpperInvariant()) is not { } productPart)
+            {
+                continue;
+            }
 
             orderLineItems.Add(new OrderLineItem(
                 lineItem.Quantity,
-                lineItem.ProductSku,
+                lineItem.ProductSku.ToUpperInvariant(),
                 lineItem.UnitPrice,
                 lineItem.LinePrice,
                 productPart.ContentItem.ContentItemVersionId
