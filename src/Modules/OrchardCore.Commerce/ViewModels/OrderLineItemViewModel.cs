b0e50e0aa8b13170ca8d9317ac4f194e49f6629c
Gábor Pór <51411356+porgabi@users.noreply.github.com>
2023-08-23 11:09:59 +0200

    OCC-65: Make the line items editable in the OrderPart editor (#327)
    * Adding base Vue app
    
    * Making products editable in Order editor
    
    * Linking to product editor in listed products
    
    * Adding dropdown for currency
    
    * Reworking HTML structure
    
    * Adjusting CSS classes
    
    * Fixing bug with multiple Price Variant Product items checkout
    
    * Validating selected currencies in driver
    
    * Adding full SKUs
    
    * Attributing
    
    * Attributing some more
    
    * Constructing Attributes in the most convoluted way humankind has ever seen
    
    * Saving selected attributes
    
    * Saving selected attributes upon successful purchase
    
    * Adding disclaimers to Order editor
    
    * Improving disclaimer
    
    * Extracting methods
    
    * Fixing Vue bug with adding new product
    
    * Removing unused variable
    
    * Removing bewilderment
    
    * Fistfighting analyzer
    
    * Renaming variable (this commit was sponsored by check-spelling-bot)
    
    * Conceding to analyzer
    
    * Removing unnecessary comment
    
    * Adding simple fixes
    
    * Adding dynamic total display
    
    * Simplifying currency display
    
    * Fixing fake validation errors when submitting form with empty row
    
    * Reworking handling of attributes
    
    * Fixing bug with multiple attributes on the same Product

--- src/Modules/OrchardCore.Commerce/ViewModels/OrderLineItemViewModel.cs
+++ src/Modules/OrchardCore.Commerce/ViewModels/OrderLineItemViewModel.cs
@@ -18,11 +18,15 @@ public class OrderLineItemViewModel : ILineItem
     public ProductPart ProductPart { get; set; }
     public int Quantity { get; set; }
     public string ProductSku { get; set; }
+    public string ProductFullSku { get; set; }
     public string ProductName { get; set; }
 
     public RouteValueDictionary ProductRouteValues { get; set; }
     public string ProductImageUrl { get; set; }
+    public decimal UnitPriceValue { get; set; }
+    public string UnitPriceCurrencyIsoCode { get; set; }
     public Amount UnitPrice { get; set; }
     public Amount LinePrice { get; set; }
     public ISet<IProductAttributeValue> Attributes { get; set; }
+    public IDictionary<string, string> SelectedAttributes { get; set; } = new Dictionary<string, string>();
 }
