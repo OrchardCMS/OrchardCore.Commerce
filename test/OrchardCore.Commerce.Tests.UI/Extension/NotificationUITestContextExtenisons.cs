6f60288b069689193070959f14cd0ace87429b06
Dávid El-Saig <david.el-saig@lombiq.com>
2023-09-12 23:49:31 +0200

    Add extension documentation.

--- test/OrchardCore.Commerce.Tests.UI/Extension/NotificationUITestContextExtenisons.cs
+++ test/OrchardCore.Commerce.Tests.UI/Extension/NotificationUITestContextExtenisons.cs
@@ -7,12 +7,23 @@ namespace Lombiq.Tests.UI.Services;
 
 public static class NotificationUITestContextExtenisons
 {
+    /// <summary>
+    /// Returns the text of the element with the <c>message-error</c> class if one exists.
+    /// </summary>
+    /// <param name="safely">
+    /// If the element is found then this doesn't matter. Otherwise if it's <see langword="true"/> then <see
+    /// langword="null"/> is returned and if it's <see langword="false"/> an exception is thrown.
+    /// </param>
     public static string GetErrorMessage(this UITestContext context, bool safely = false)
     {
         var by = By.ClassName("message-error").Safely(safely);
         return context.Get(by)?.Text?.Trim();
     }
 
+    /// <summary>
+    /// Looks for the element with the <c>message-error</c> class, it shouldn't exist or its content should be empty. If
+    /// that's not true an exception will be thrown containing the element text.
+    /// </summary>
     public static void ErrorMessageShouldNotExist(this UITestContext context) =>
-        context.GetErrorMessage(safely: true).ShouldBeNull();
+        context.GetErrorMessage(safely: true).ShouldBeNullOrEmpty();
 }
