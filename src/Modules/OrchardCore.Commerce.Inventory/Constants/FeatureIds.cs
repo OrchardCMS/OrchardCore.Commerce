23a80671e8a745395dc90c43c7372ffb0ad0af9d
Dávid El-Saig <david.el-saig@lombiq.com>
2023-09-13 12:47:19 +0200

    Add Feature ID for "Orchard Core Commerce - Inventory".

---
+++ src/Modules/OrchardCore.Commerce.Inventory/Constants/FeatureIds.cs
@@ -0,0 +1,7 @@
+﻿namespace OrchardCore.Commerce.Inventory.Constants;
+
+public static class FeatureIds
+{
+    public const string Area = "OrchardCore.Commerce.Inventory";
+    public const string Inventory = Area;
+}
