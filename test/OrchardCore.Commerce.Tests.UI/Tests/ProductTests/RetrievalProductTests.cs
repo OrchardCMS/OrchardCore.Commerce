c0847b0591e469ca9de047fe5a501a415696fb32
Szabolcs Deme <szabolcs.deme@lombiq.com>
2023-09-21 19:21:57 +0200

    Adding UI test

--- test/OrchardCore.Commerce.Tests.UI/Tests/ProductTests/RetrievalProductTests.cs
+++ test/OrchardCore.Commerce.Tests.UI/Tests/ProductTests/RetrievalProductTests.cs
@@ -1,4 +1,4 @@
-﻿using Lombiq.Tests.UI.Attributes;
+using Lombiq.Tests.UI.Attributes;
 using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Services;
 using OpenQA.Selenium;
@@ -26,8 +26,21 @@ public class RetrievalProductTests : UITestBase
                 await context.SignInDirectlyAndGoToHomepageAsync();
 
                 TextShouldBe(".site-heading h1", "My Shop");
+                TextShouldBe(".site-heading h1", "My Shop");
                 TextShouldBe(".content-price-variants-product header h2 a", "Test Price Variant Product");
+                TextShouldBe(
+                    ".content-price-variants-product > .field-name-product-" +
+                    "part-product-image > .name",
+                    "Product Image");
                 TextShouldBe(".content-product header h2 a", "Test Product");
+                TextShouldBe(
+                    ".content-product > .field-name-product-" +
+                    "part-product-image > .name",
+                    "Product Image");
+
+                context.GetAll(By.XPath("//img[@src='/media/ProductImages/sample-product-image.png']"))
+                    .Count
+                    .ShouldBe(5);
             },
             browser);
 }
