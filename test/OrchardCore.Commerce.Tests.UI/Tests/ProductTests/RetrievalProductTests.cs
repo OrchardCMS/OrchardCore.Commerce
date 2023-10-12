bd598eab4350d3a2c77f43d0808d7032d0f6b0d2
Gábor Pór <gabor.por@lombiq.com>
2023-10-12 15:26:13 +0200

    Adding UI test

--- test/OrchardCore.Commerce.Tests.UI/Tests/ProductTests/RetrievalProductTests.cs
+++ test/OrchardCore.Commerce.Tests.UI/Tests/ProductTests/RetrievalProductTests.cs
@@ -5,6 +5,7 @@ using OpenQA.Selenium;
 using Shouldly;
 using Xunit;
 using Xunit.Abstractions;
+using static OrchardCore.Commerce.Tests.UI.Constants.ContentItemIds;
 
 namespace OrchardCore.Commerce.Tests.UI.Tests.ProductTests;
 
@@ -40,6 +41,85 @@ public class RetrievalProductTests : UITestBase
                 context.GetAll(By.XPath("//img[@src='/media/ProductImages/sample-product-image.png']"))
                     .Count
                     .ShouldBe(5);
+            },
+            browser);
+
+    [Theory, Chrome]
+    public Task ProductAttributesShouldBeDisplayedCorrectlyOnProductPage(Browser browser) =>
+        ExecuteTestAfterSetupAsync(
+            async context =>
+            {
+                await context.SignInDirectlyAsync();
+                await context.GoToAdminRelativeUrlAsync("/ContentParts/PriceVariantsProduct/Fields/Size/Edit");
+                await context.ClickAndFillInWithRetriesAsync(
+                    By.Id("PriceVariantsProduct_Size_TextProductAttributeFieldSettingsDriver_Hint"),
+                    "Test Text Hint");
+                await context.ClickReliablyOnSubmitAsync();
+
+                // Verify text attribute properties show up correctly.
+                await context.GoToContentItemByIdAsync(TestPriceVariantProduct);
+                await context.ClickReliablyOnAsync(By.XPath("//label[contains(., 'Medium')]"));
+                context.Exists(By.XPath("//div[contains(., 'Test Boolean Hint')]"));
+                await context.ClickReliablyOnSubmitAsync();
+
+                context.Exists(By.XPath("//li[contains(., 'Size: Medium')]"));
+                await context.ClickReliablyOnAsync(By.XPath("//button[contains(., 'Remove')]"));
+
+                // Add new boolean attribute and verify its properties show up correctly.
+                await context.GoToAdminRelativeUrlAsync("/ContentTypes/AddFieldsTo/PriceVariantsProduct");
+                await context.ClickAndFillInWithRetriesAsync(By.Id("DisplayName"), "TestBooleanAttribute");
+                await context.ClickReliablyOnAsync(By.XPath("//label[contains(., 'Boolean Product Attribute Field')]"));
+                await context.ClickReliablyOnSubmitAsync();
+
+                await context.ClickAndFillInWithRetriesAsync(
+                    By.Id("PriceVariantsProduct_TestBooleanAttribute_BooleanProductAttributeFieldSettingsDriver_Hint"),
+                    "Test Boolean Hint");
+                await context.ClickAndFillInWithRetriesAsync(
+                    By.Id("PriceVariantsProduct_TestBooleanAttribute_BooleanProductAttributeFieldSettingsDriver_Label"),
+                    "Test Boolean Label");
+                await context.ClickReliablyOnAsync(By.XPath("//label[contains(., 'On/Off')]"));
+                await context.ClickReliablyOnSubmitAsync();
+
+                await context.GoToContentItemByIdAsync(TestPriceVariantProduct);
+                context.Exists(By.XPath("//label[contains(., 'Test Boolean Label')]"));
+                context.Exists(By.XPath("//div[contains(., 'Test Boolean Hint')]"));
+                await context.ClickReliablyOnSubmitAsync();
+
+                context.Exists(By.XPath("//li[contains(., 'Size: Small')]"));
+                context.Exists(By.XPath("//li[contains(., 'TestBooleanAttribute')]"));
+                await context.ClickReliablyOnAsync(By.XPath("//button[contains(., 'Remove')]"));
+
+                // Add new numeric attribute and verify its properties show up correctly.
+                await context.GoToAdminRelativeUrlAsync("/ContentTypes/AddFieldsTo/PriceVariantsProduct");
+                await context.ClickAndFillInWithRetriesAsync(By.Id("DisplayName"), "TestNumericAttribute");
+                await context.ClickReliablyOnAsync(By.XPath("//label[contains(., 'Numeric Product Attribute Field')]"));
+                await context.ClickReliablyOnSubmitAsync();
+
+                await context.ClickAndFillInWithRetriesAsync(
+                    By.Id("PriceVariantsProduct_TestNumericAttribute_NumericProductAttributeFieldSettingsDriver_Hint"),
+                    "Test Numeric Hint");
+                await context.ClickAndFillInWithRetriesAsync(
+                    By.Id("PriceVariantsProduct_TestNumericAttribute_NumericProductAttributeFieldSettingsDriver_Placeholder"),
+                    "Test Numeric Placeholder");
+                await context.ClickAndFillInWithRetriesAsync(
+                    By.Id("PriceVariantsProduct_TestNumericAttribute_NumericProductAttributeFieldSettingsDriver_Minimum"),
+                    "1");
+                await context.ClickAndFillInWithRetriesAsync(
+                    By.Id("PriceVariantsProduct_TestNumericAttribute_NumericProductAttributeFieldSettingsDriver_Maximum"),
+                    "2");
+                await context.ClickReliablyOnSubmitAsync();
+
+                await context.GoToContentItemByIdAsync(TestPriceVariantProduct);
+                context.Exists(By.XPath("//div[contains(., 'Test Numeric Hint')]"));
+                context
+                    .Get(By.Id("product-TESTPRODUCTVARIANT-attribute-2"))
+                    .GetAttribute("placeholder")
+                    .ShouldBe("Test Numeric Placeholder");
+                await context.ClickAndFillInWithRetriesAsync(
+                    By.Id("product-TESTPRODUCTVARIANT-attribute-2"),
+                    "2");
+                await context.ClickReliablyOnSubmitAsync();
+                context.Exists(By.XPath("//li[contains(., 'TestNumericAttribute: 2')]"));
             },
             browser);
 }
