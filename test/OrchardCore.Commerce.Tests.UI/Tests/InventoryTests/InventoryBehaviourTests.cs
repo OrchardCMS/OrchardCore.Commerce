d53c56d32c51a8f55b1f7f89185f42df7b119943
Gábor Pór <gabor.por@lombiq.com>
2023-11-15 13:00:06 +0100

    Improving test

--- test/OrchardCore.Commerce.Tests.UI/Tests/InventoryTests/InventoryBehaviourTests.cs
+++ test/OrchardCore.Commerce.Tests.UI/Tests/InventoryTests/InventoryBehaviourTests.cs
@@ -33,7 +33,7 @@ public class InventoryBehaviourTests : UITestBase
                     "The checkout quantity for Test Product is more than the maximum allowed (2).",
                     shouldExist: false);
 
-                await context.GoToAdminRelativeUrlAsync($"/Contents/ContentItems/{TestProduct}/Edit");
+                await context.GoToContentItemEditorByIdAsync(TestProduct);
                 await context.ClickReliablyOnAsync(By.Id("InventoryPart_AllowsBackOrder_Value"));
                 await context.ClickAndFillInWithRetriesAsync(By.Id("InventoryPart_MinimumOrderQuantity_Value"), "3");
                 await context.ClickAndFillInWithRetriesAsync(By.Id("InventoryPart_MaximumOrderQuantity_Value"), "10");
@@ -54,7 +54,7 @@ public class InventoryBehaviourTests : UITestBase
                     "There are not enough Test Product left in stock.",
                     shouldExist: true);
 
-                await context.GoToAdminRelativeUrlAsync($"/Contents/ContentItems/{TestProduct}/Edit");
+                await context.GoToContentItemEditorByIdAsync(TestProduct);
                 await context.ClickReliablyOnAsync(By.Id("InventoryPart_IgnoreInventory_Value"));
                 await context.ClickPublishAsync();
                 await context.GoToCartAsync();
@@ -83,47 +83,51 @@ public class InventoryBehaviourTests : UITestBase
                 await context.SignInDirectlyAndGoToRelativeUrlAsync("/testproduct");
                 await context.ClickReliablyOnAsync(By.XPath("//button[contains(., 'Add to cart')]"));
 
-                // Set Inventory of product in cart to 0.
-                await context.GoToAdminRelativeUrlAsync($"/Contents/ContentItems/{TestProduct}/Edit");
+                // Set Inventory of product in cart to 0 and verify checkout is unavailable.
+                await context.GoToContentItemEditorByIdAsync(TestProduct);
                 await context.ClickReliablyOnAsync(By.XPath("//label[contains(., 'Allows Back Order')]"));
                 await context.ClickAndFillInWithRetriesAsync(By.Name("InventoryPart.Inventory[TESTPRODUCT]"), "0");
                 await context.ClickPublishAsync();
 
-                // Verify checkout is unavailable.
-                await context.GoToCartAsync();
-                context.Exists(By.XPath($"//p[contains(., '{CheckoutUnavailableMessage}')]"));
-                await context.GoToCheckoutAsync();
-                context.Exists(By.XPath($"//div[contains(., '{CheckoutUnavailableMessage}')]"));
+                await VerifyCheckoutAvailabilityAsync(context, cartMessageShouldExist: true, checkoutMessageShouldExist: true);
 
                 // Add an available product to cart and verify checkout is still not possible.
                 await context.GoToRelativeUrlAsync("testfreeproduct");
                 await context.ClickReliablyOnAsync(By.XPath("//button[contains(., 'Add to cart')]"));
-                context.Exists(By.XPath($"//p[contains(., '{CheckoutUnavailableMessage}')]"));
-                await context.GoToCheckoutAsync();
-                context.Exists(By.XPath($"//div[contains(., '{CheckoutUnavailableMessage}')]"));
+                await VerifyCheckoutAvailabilityAsync(
+                    context, cartMessageShouldExist: true, checkoutMessageShouldExist: true, navigate: false);
 
                 // Set Ignore Inventory to true and verify checkout is available.
-                await context.GoToAdminRelativeUrlAsync($"/Contents/ContentItems/{TestProduct}/Edit");
+                await context.GoToContentItemEditorByIdAsync(TestProduct);
                 await context.ClickReliablyOnAsync(By.XPath("//label[contains(., 'Ignore Inventory')]"));
                 await context.ClickPublishAsync();
 
-                await context.GoToCartAsync();
-                context.Missing(By.XPath($"//p[contains(., '{CheckoutUnavailableMessage}')]"));
-                await context.GoToCheckoutAsync();
-                context.Missing(By.XPath($"//div[contains(., '{CheckoutUnavailableMessage}')]"));
+                await VerifyCheckoutAvailabilityAsync(context, cartMessageShouldExist: false, checkoutMessageShouldExist: true);
 
                 // Set Allows Back Order to true and verify checkout is available.
-                await context.GoToAdminRelativeUrlAsync($"/Contents/ContentItems/{TestProduct}/Edit");
+                await context.GoToContentItemEditorByIdAsync(TestProduct);
                 await context.ClickReliablyOnAsync(By.XPath("//label[contains(., 'Ignore Inventory')]"));
                 await context.ClickReliablyOnAsync(By.XPath("//label[contains(., 'Allows Back Order')]"));
                 await context.ClickPublishAsync();
 
-                await context.GoToCartAsync();
-                context.Missing(By.XPath($"//p[contains(., '{CheckoutUnavailableMessage}')]"));
-                await context.GoToCheckoutAsync();
-                context.Missing(By.XPath($"//div[contains(., '{CheckoutUnavailableMessage}')]"));
+                await VerifyCheckoutAvailabilityAsync(context, cartMessageShouldExist: false, checkoutMessageShouldExist: true);
             },
             browser);
+
+    private static async Task VerifyCheckoutAvailabilityAsync(
+        UITestContext context, bool cartMessageShouldExist, bool checkoutMessageShouldExist, bool navigate = true)
+    {
+        if (navigate)
+        {
+            await context.GoToCartAsync();
+        }
+
+        context.CheckExistence(By.ClassName("checkout-unavailable"), cartMessageShouldExist);
+        await context.GoToCheckoutAsync();
+        context.CheckExistence(
+            By.XPath($"//div[contains(@class, 'message-error') and contains(., '{CheckoutUnavailableMessage}')]"),
+            checkoutMessageShouldExist);
+    }
 
     private static async Task UpdateCartAndAssertErrorsAsync(
         UITestContext context,
