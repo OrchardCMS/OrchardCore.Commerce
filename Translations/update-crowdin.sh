243eac5538949d5fac19ae631be3e14f269c92e8
Sára El-Saig <sara.el-saig@lombiq.com>
2023-11-16 17:59:19 +0100

    Add Crowdin upload script and config template.

---
+++ Translations/update-crowdin.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+PROJECT_ID="$1"
+PERSONAL_TOKEN="$2"
+
+# Initialize the configuration file from the template.
+echo "\"project_id\": \"$PROJECT_ID\"" > crowdin.yml
+echo "\"api_token\": \"$PERSONAL_TOKEN\"" >> crowdin.yml
+cat crowdin.yml.template >> crowdin.yml
+
+crowdin upload sources
