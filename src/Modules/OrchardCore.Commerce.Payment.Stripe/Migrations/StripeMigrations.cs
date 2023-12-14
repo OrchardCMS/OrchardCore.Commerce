a6c82fba07f059c0b257a60d7e0370b89409defa
Krisztián Németh <krisztian.nemeth@lombiq.com>
2023-12-14 13:25:06 +0100

    Adding logging

--- src/Modules/OrchardCore.Commerce.Payment.Stripe/Migrations/StripeMigrations.cs
+++ src/Modules/OrchardCore.Commerce.Payment.Stripe/Migrations/StripeMigrations.cs
@@ -1,10 +1,12 @@
 using Lombiq.HelpfulLibraries.OrchardCore.Data;
 using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Logging;
 using OrchardCore.Commerce.Payment.Stripe.Indexes;
 using OrchardCore.Commerce.Payment.Stripe.Models;
 using OrchardCore.ContentManagement.Metadata;
 using OrchardCore.ContentManagement.Metadata.Settings;
 using OrchardCore.Data.Migration;
+using System;
 using YesSql.Sql;
 using static OrchardCore.Commerce.Abstractions.Constants.ContentTypes;
 
@@ -13,9 +15,13 @@ namespace OrchardCore.Commerce.Payment.Stripe.Migrations;
 public class StripeMigrations : DataMigration
 {
     private readonly IContentDefinitionManager _contentDefinitionManager;
+    private readonly ILogger<StripeMigrations> _logger;
 
-    public StripeMigrations(IContentDefinitionManager contentDefinitionManager) =>
+    public StripeMigrations(IContentDefinitionManager contentDefinitionManager, ILogger<StripeMigrations> logger)
+    {
         _contentDefinitionManager = contentDefinitionManager;
+        _logger = logger;
+    }
 
     public int Create()
     {
@@ -33,9 +39,13 @@ public class StripeMigrations : DataMigration
         {
             SchemaBuilder.DropMapIndexTable(typeof(OrderPaymentIndex));
         }
-        catch (SqlException)
+        catch (Exception exception) when (exception is SqlException)
         {
             // This is fine, it just means that the table didn't exist.
+            _logger.LogInformation(
+                "The table {TableName} didn't exist so it couldn't be dropped, full exception message: {ExceptionMessage}",
+                typeof(OrderPaymentIndex),
+                exception.Message);
         }
 
         SchemaBuilder
