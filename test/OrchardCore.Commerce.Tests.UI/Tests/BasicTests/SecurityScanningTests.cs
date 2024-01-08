c6b8610528434e3c6da4bf0c8a18ae88a481dcef
Sára El-Saig <sara.el-saig@lombiq.com>
2024-01-08 09:23:36 +0100

    Add security scanning test.

---
+++ test/OrchardCore.Commerce.Tests.UI/Tests/BasicTests/SecurityScanningTests.cs
@@ -0,0 +1,22 @@
+﻿using Lombiq.Tests.UI.SecurityScanning;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace OrchardCore.Commerce.Tests.UI.Tests.BasicTests;
+
+public class SecurityScanningTests : UITestBase
+{
+    public SecurityScanningTests(ITestOutputHelper testOutputHelper)
+        : base(testOutputHelper)
+    {
+    }
+
+    [Fact]
+    public Task FullSecurityScanShouldPass() =>
+        ExecuteTestAfterSetupAsync(context =>
+            context.RunAndConfigureAndAssertFullSecurityScanForAutomationAsync(ConfigureSecurityScan));
+
+    private static void ConfigureSecurityScan(SecurityScanConfiguration configuration)
+    {
+    }
+}
