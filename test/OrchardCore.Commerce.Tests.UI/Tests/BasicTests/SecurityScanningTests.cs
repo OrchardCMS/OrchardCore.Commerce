0553ab1782af97b3189bd7723450b7ef7067ca7d
Sára El-Saig <sara.el-saig@lombiq.com>
2024-01-08 18:08:43 +0100

    Exclude the rule for real.

--- test/OrchardCore.Commerce.Tests.UI/Tests/BasicTests/SecurityScanningTests.cs
+++ test/OrchardCore.Commerce.Tests.UI/Tests/BasicTests/SecurityScanningTests.cs
@@ -14,8 +14,12 @@ public class SecurityScanningTests : UITestBase
     [Fact]
     public Task FullSecurityScanShouldPass() =>
         ExecuteTestAfterSetupAsync(context => context.RunAndConfigureAndAssertFullSecurityScanForAutomationAsync(configuration =>
-            configuration.MarkScanRuleAsFalsePositiveForUrlWithRegex(
-                @"https://[^/]+/(\?.*pagenum=[0-9]+&.*)?(\?.*products\..*)?",
-                10202,
-                "Absence of Anti-CSRF Tokens: The product list filters are intentionally in a GET form. No XSS risk here.")));
+        {
+            const string antiCsrfJustification =
+                "Absence of Anti-CSRF Tokens: The ProductListPart-Filters intentionally uses a GET form. No XSS risk.";
+
+            configuration.MarkScanRuleAsFalsePositiveForUrlWithRegex(@"https://[^/]+/", 10202, antiCsrfJustification);
+            configuration.MarkScanRuleAsFalsePositiveForUrlWithRegex(@".*/\?.*pagenum=.*", 10202, antiCsrfJustification);
+            configuration.MarkScanRuleAsFalsePositiveForUrlWithRegex(@".*/\?.*products\..*", 10202, antiCsrfJustification);
+        }));
 }
