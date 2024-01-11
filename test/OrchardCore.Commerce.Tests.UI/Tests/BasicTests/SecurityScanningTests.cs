6d05307c660b3e6ea7371fc7e99dd90a0ac2c90c
Sára El-Saig <sara.el-saig@lombiq.com>
2024-01-12 00:34:48 +0100

    Exclude SecurityScanningTests from Windows runner.

--- test/OrchardCore.Commerce.Tests.UI/Tests/BasicTests/SecurityScanningTests.cs
+++ test/OrchardCore.Commerce.Tests.UI/Tests/BasicTests/SecurityScanningTests.cs
@@ -18,43 +18,37 @@ public class SecurityScanningTests : UITestBase
 
     [Fact]
     public Task FullSecurityScanShouldPass() =>
-        // On GitHub when running from Windows, this test always fails with the following error:
-        // The `docker.exe pull softwaresecurityproject/zap-stable:2.14.0 --quiet` command failed with the output below.
-        // no matching manifest for windows/amd64 10.0.20348 in the manifest list entries
-        // This is because the Docker on the runner is in Windows mode but zap-stable only has Linux images.
-        GitHubHelper.IsGitHubEnvironment && OSInfo.IsWindows
-            ? Task.CompletedTask
-            : ExecuteTestAfterSetupAsync(
-                context => context.RunAndConfigureAndAssertFullSecurityScanForAutomationAsync(
-                    configuration => FalsePositive(
-                        configuration,
-                        10202,
-                        "Absence of Anti-CSRF Tokens: The ProductListPart-Filters intentionally uses a GET form. No XSS risk.",
-                        @"https://[^/]+/",
-                        @".*/\?.*pagenum=.*",
-                        @".*/\?.*products\..*"),
-                    sarifLog =>
-                    {
-                        var errors = sarifLog
-                            .Runs[0]
-                            .Results
-                            .Where(result =>
-                                result.Kind == ResultKind.Fail &&
-                                result.Level != FailureLevel.None &&
-                                result.Level != FailureLevel.Note &&
-                                // Exclude the specific false positive that was already excluded above in the configuration.
-                                // See https://github.com/Lombiq/UI-Testing-Toolbox/issues/336 for more details.
-                                result.Locations?.Any(location =>
-                                    location.PhysicalLocation?.Region?.Snippet?.Text == "<form method=\"get\" action=\"/\">") != true)
-                            .Select(result => new
-                            {
-                                Kind = result.Kind.ToString(),
-                                Level = result.Level.ToString(),
-                                Details = result,
-                            })
-                            .ToList();
-                        errors.ShouldBeEmpty(JsonConvert.SerializeObject(errors));
-                    }));
+        ExecuteTestAfterSetupAsync(
+            context => context.RunAndConfigureAndAssertFullSecurityScanForAutomationAsync(
+                configuration => FalsePositive(
+                    configuration,
+                    10202,
+                    "Absence of Anti-CSRF Tokens: The ProductListPart-Filters intentionally uses a GET form. No XSS risk.",
+                    @"https://[^/]+/",
+                    @".*/\?.*pagenum=.*",
+                    @".*/\?.*products\..*"),
+                sarifLog =>
+                {
+                    var errors = sarifLog
+                        .Runs[0]
+                        .Results
+                        .Where(result =>
+                            result.Kind == ResultKind.Fail &&
+                            result.Level != FailureLevel.None &&
+                            result.Level != FailureLevel.Note &&
+                            // Exclude the specific false positive that was already excluded above in the configuration.
+                            // See https://github.com/Lombiq/UI-Testing-Toolbox/issues/336 for more details.
+                            result.Locations?.Any(location =>
+                                location.PhysicalLocation?.Region?.Snippet?.Text == "<form method=\"get\" action=\"/\">") != true)
+                        .Select(result => new
+                        {
+                            Kind = result.Kind.ToString(),
+                            Level = result.Level.ToString(),
+                            Details = result,
+                        })
+                        .ToList();
+                    errors.ShouldBeEmpty(JsonConvert.SerializeObject(errors));
+                }));
 
     private static void FalsePositive(
         SecurityScanConfiguration configuration,
