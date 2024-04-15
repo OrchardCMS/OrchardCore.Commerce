747434988598ea6e4972539b2c4ffb95c9ca289c
Sára El-Saig <sara.el-saig@lombiq.com>
2024-04-15 14:22:28 +0200

    Add StripeContentSecurityPolicyProvider.

---
+++ src/Modules/OrchardCore.Commerce.Payment.Stripe/Services/StripeContentSecurityPolicyProvider.cs
@@ -0,0 +1,25 @@
+﻿using Lombiq.HelpfulLibraries.AspNetCore.Security;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using static Lombiq.HelpfulLibraries.AspNetCore.Security.ContentSecurityPolicyDirectives;
+
+namespace OrchardCore.Commerce.Payment.Stripe.Services;
+
+public class StripeContentSecurityPolicyProvider : IContentSecurityPolicyProvider
+{
+    public ValueTask UpdateAsync(IDictionary<string, string> securityPolicies, HttpContext context)
+    {
+        securityPolicies[ScriptSrc] = IContentSecurityPolicyProvider
+            .GetDirective(securityPolicies, ScriptSrc)
+            .MergeWordSets("https://js.stripe.com/v3/");
+
+        securityPolicies[FrameSrc] = IContentSecurityPolicyProvider
+            .GetDirective(securityPolicies, FrameSrc)
+            .MergeWordSets("https://js.stripe.com/v3/");
+
+        return ValueTask.CompletedTask;
+    }
+}
