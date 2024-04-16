db85d6a1da35c4057f0ec760447e49514ab3bcc3
Sára El-Saig <sara.el-saig@lombiq.com>
2024-04-16 14:57:55 +0200

    OCC-233: Add StripeContentSecurityPolicyProvider (#425)
    Co-authored-by: Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>

---
+++ src/Modules/OrchardCore.Commerce.Payment.Stripe/Services/StripeContentSecurityPolicyProvider.cs
@@ -0,0 +1,25 @@
+﻿using Lombiq.HelpfulLibraries.AspNetCore.Security;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using static Lombiq.HelpfulLibraries.AspNetCore.Security.ContentSecurityPolicyDirectives;
+
+namespace OrchardCore.Commerce.Payment.Stripe.Services;
+
+public class StripeContentSecurityPolicyProvider : IContentSecurityPolicyProvider
+{
+    public ValueTask UpdateAsync(IDictionary<string, string> securityPolicies, HttpContext context)
+    {
+        securityPolicies[ScriptSrc] = ContentSecurityPolicyProvider
+            .GetDirective(securityPolicies, ScriptSrc)
+            .MergeWordSets("https://js.stripe.com/");
+
+        securityPolicies[FrameSrc] = ContentSecurityPolicyProvider
+            .GetDirective(securityPolicies, FrameSrc)
+            .MergeWordSets("https://js.stripe.com/");
+
+        return ValueTask.CompletedTask;
+    }
+}
