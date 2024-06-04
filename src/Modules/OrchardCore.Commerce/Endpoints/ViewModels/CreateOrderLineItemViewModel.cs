4a6657bf682f27ff5b98bc16d423c08c6ca97962
Andy <zgofmontreal@gmail.com>
2024-06-03 21:16:41 -0400

    use { get; init; } for IList<OrderLineItem> LineItems

--- src/Modules/OrchardCore.Commerce/Endpoints/ViewModels/CreateOrderLineItemViewModel.cs
+++ src/Modules/OrchardCore.Commerce/Endpoints/ViewModels/CreateOrderLineItemViewModel.cs
@@ -4,8 +4,6 @@ using System.Collections.Generic;
 namespace OrchardCore.Commerce.Endpoints.ViewModels;
 public class CreateOrderLineItemViewModel
 {
-#pragma warning disable CA2227 // 集合属性应为只读
-    public IList<OrderLineItem> LineItems { get; set; }
-#pragma warning restore CA2227 // 集合属性应为只读
+    public IList<OrderLineItem> LineItems { get; init; }
     public OrderPart OrderPart { get; set; }
 }
