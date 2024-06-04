99f03ddeccdc838a57cb4c18774e82b3174d7cde
Andy <zgofmontreal@gmail.com>
2024-06-03 21:26:28 -0400

    delete .prettierrc.js

--- src/Modules/OrchardCore.Commerce.Payment.Stripe/.prettierrc.js
+++
@@ -1,7 +0,0 @@
-// The following path may have to be adjusted to your directory structure.
-const lombiqConfig = require('./node_modules/nodejs-extensions/config/.prettierrc.lombiq-base.js');
-
-module.exports = {
-    ...lombiqConfig,
-    // Add custom rules and overrides here.
-}
