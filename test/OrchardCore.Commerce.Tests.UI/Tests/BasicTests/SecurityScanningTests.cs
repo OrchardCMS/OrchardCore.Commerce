4320fff7b2e58175915e770f6cce24979922ba4c
Sára El-Saig <sara.el-saig@lombiq.com>
2024-07-15 13:13:51 +0200

    See if we can force timeout using Task.WhenAny

--- test/OrchardCore.Commerce.Tests.UI/Tests/BasicTests/SecurityScanningTests.cs
+++ test/OrchardCore.Commerce.Tests.UI/Tests/BasicTests/SecurityScanningTests.cs
@@ -1,7 +1,6 @@
 using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.SecurityScanning;
 using Shouldly;
-using System.Diagnostics.CodeAnalysis;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -14,46 +13,47 @@ public class SecurityScanningTests : UITestBase
     {
     }
 
-    [Fact(Skip = "Testing if this test is really the only thing that fails and hangs.")]
-    [SuppressMessage("Usage", "xUnit1004:Test methods should not be skipped", Justification = "See above.")]
+    [Fact]
     public Task FullSecurityScanShouldPass() =>
-        ExecuteTestAfterSetupAsync(
-            context => context.RunAndConfigureAndAssertFullSecurityScanForContinuousIntegrationAsync(
-                configuration =>
-                {
-                    configuration.DisableActiveScanRule(
-                        6,
-                        "Path Traversal (all paths are virtual so it's not a real concern, also creates too many errors)");
+        Task.WhenAny(
+            Task.Delay(TimeSpan.FromMinutes(1)).ContinueWith(_ => throw new TimeoutException("The security test timeout has elapsed."), TaskScheduler.Default),
+            ExecuteTestAfterSetupAsync(
+                    context => context.RunAndConfigureAndAssertFullSecurityScanForContinuousIntegrationAsync(
+                        configuration =>
+                        {
+                            configuration.DisableActiveScanRule(
+                                6,
+                                "Path Traversal (all paths are virtual so it's not a real concern, also creates too many errors)");
 
-                    configuration.DisableActiveScanRule(
-                        40024,
-                        "SQL Injection - SQLite (everything goes through YesSql so these are false positive)");
+                            configuration.DisableActiveScanRule(
+                                40024,
+                                "SQL Injection - SQLite (everything goes through YesSql so these are false positive)");
 
-                    configuration.DisableActiveScanRule(
-                        40027,
-                        "The query time is controllable using parameter value [some SQL injection]");
+                            configuration.DisableActiveScanRule(
+                                40027,
+                                "The query time is controllable using parameter value [some SQL injection]");
 
-                    FalsePositive(
-                        configuration,
-                        10202,
-                        "Absence of Anti-CSRF Tokens",
-                        "The ProductListPart-Filters intentionally uses a GET form. No XSS risk.",
-                        @"https://[^/]+/",
-                        @".*/\?.*pagenum=.*",
-                        @".*/\?.*products\..*");
-                }),
-            changeConfiguration: configuration => configuration.AssertAppLogsAsync = async webApplicationInstance =>
-            {
-                var logsWithoutUnwantedExceptionMessages = (await webApplicationInstance.GetLogOutputAsync())
-                    .SplitByNewLines()
-                    .Where(message =>
-                        !message.ContainsOrdinalIgnoreCase("System.IO.DirectoryNotFoundException: Could not find a part of the path") &&
-                        !message.ContainsOrdinalIgnoreCase(
-                            "System.IO.IOException: The filename, directory name, or volume label syntax is incorrect") &&
-                        !message.ContainsOrdinalIgnoreCase("System.InvalidOperationException: This action intentionally causes an exception!"));
+                            FalsePositive(
+                                configuration,
+                                10202,
+                                "Absence of Anti-CSRF Tokens",
+                                "The ProductListPart-Filters intentionally uses a GET form. No XSS risk.",
+                                @"https://[^/]+/",
+                                @".*/\?.*pagenum=.*",
+                                @".*/\?.*products\..*");
+                        }),
+                    changeConfiguration: configuration => configuration.AssertAppLogsAsync = async webApplicationInstance =>
+                    {
+                        var logsWithoutUnwantedExceptionMessages = (await webApplicationInstance.GetLogOutputAsync())
+                            .SplitByNewLines()
+                            .Where(message =>
+                                !message.ContainsOrdinalIgnoreCase("System.IO.DirectoryNotFoundException: Could not find a part of the path") &&
+                                !message.ContainsOrdinalIgnoreCase(
+                                    "System.IO.IOException: The filename, directory name, or volume label syntax is incorrect") &&
+                                !message.ContainsOrdinalIgnoreCase("System.InvalidOperationException: This action intentionally causes an exception!"));
 
-                logsWithoutUnwantedExceptionMessages.ShouldNotContain(item => item.Contains("|ERROR|"));
-            });
+                        logsWithoutUnwantedExceptionMessages.ShouldNotContain(item => item.Contains("|ERROR|"));
+                    }));
 
     private static void FalsePositive(
         SecurityScanConfiguration configuration,
