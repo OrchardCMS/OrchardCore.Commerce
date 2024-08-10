76d67b65f3296243702bc8ecad94755ff8eed803
Andy <zgofmontreal@gmail.com>
2024-08-10 16:41:22 -0400

    make HideMessage stronger

--- src/Modules/OrchardCore.Commerce.Payment/ViewModels/PaidStatusViewModel.cs
+++ src/Modules/OrchardCore.Commerce.Payment/ViewModels/PaidStatusViewModel.cs
@@ -4,9 +4,15 @@ using OrchardCore.ContentManagement;
 namespace OrchardCore.Commerce.Payment.ViewModels;
 public class PaidStatusViewModel
 {
+    private string? _hideMessage;
+    public string? HideMessage
+    {
+        get => string.IsNullOrEmpty(_hideMessage) ? ShowMessage?.Value : _hideMessage;
+        set => _hideMessage = value;
+    }
+
     public PaidStatus Status { get; set; }
     public LocalizedHtmlString? ShowMessage { get; set; }
-    public string? HideMessage { get; set; }
-    public string Url { get; set; } = string.Empty;
+    public string? Url { get; set; }
     public ContentItem? Content { get; set; }
 }
