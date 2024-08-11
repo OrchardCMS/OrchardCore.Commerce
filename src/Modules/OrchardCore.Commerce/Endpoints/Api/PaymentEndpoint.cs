5087f74f251ea1711b763913926b7c5388013789
Andy <zgofmontreal@gmail.com>
2024-08-10 20:45:59 -0400

    Add api/checkout/free

---
+++ src/Modules/OrchardCore.Commerce/Endpoints/Api/PaymentEndpoint.cs
@@ -0,0 +1,47 @@
+#nullable enable
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.AspNetCore.Routing;
+using OrchardCore.Commerce.Endpoints.Permissions;
+using OrchardCore.Commerce.Payment.Abstractions;
+using OrchardCore.Modules;
+using System.Threading.Tasks;
+
+namespace OrchardCore.Commerce.Endpoints.Api;
+public static class PaymentEndpoint
+{
+    public static IEndpointRouteBuilder AddFreeEndpoint(this IEndpointRouteBuilder builder)
+    {
+        builder.MapPost("api/checkout/free/{shoppingCartId?}", AddFreeAsync)
+            .AllowAnonymous()
+            .DisableAntiforgery();
+
+        return builder;
+    }
+
+    [Authorize(AuthenticationSchemes = "Api")]
+    private static async Task<IResult> AddFreeAsync(
+         string? shoppingCartId,
+         IAuthorizationService authorizationService,
+         HttpContext httpContext,
+         IHtmlLocalizer<IPaymentService> htmlLocalizer,
+         IPaymentService paymentService
+       )
+    {
+        if (!await authorizationService.AuthorizeAsync(httpContext.User, ApiPermissions.CommerceApi))
+        {
+            return httpContext.ChallengeOrForbid("Api");
+        }
+
+        if (await paymentService.CreatePendingOrderFromShoppingCartAsync(shoppingCartId, mustBeFree: true) is { } order)
+        {
+            var result = await paymentService.UpdateAndRedirectToFinishedOrderAsync(order, shoppingCartId, htmlLocalizer);
+            return TypedResults.Ok(result);
+        }
+
+        return TypedResults.NotFound();
+    }
+}
