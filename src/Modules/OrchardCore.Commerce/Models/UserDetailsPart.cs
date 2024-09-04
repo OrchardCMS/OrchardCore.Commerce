8c11bc793aab47234556ff0e9fe932f5cc7de10c
Andy <zgofmontreal@gmail.com>
2024-09-04 11:03:21 -0400

    Fixes #492

--- src/Modules/OrchardCore.Commerce/Models/UserDetailsPart.cs
+++ src/Modules/OrchardCore.Commerce/Models/UserDetailsPart.cs
@@ -3,7 +3,9 @@ using OrchardCore.ContentManagement;
 
 namespace OrchardCore.Commerce.Models;
 
-public class UserDetailsPart : ContentPart
+#pragma warning disable S2333 // Redundant modifiers should not be used
+public partial class UserDetailsPart : ContentPart
+#pragma warning restore S2333 // Redundant modifiers should not be used
 {
     public TextField PhoneNumber { get; set; } = new();
     public TextField VatNumber { get; set; } = new();
