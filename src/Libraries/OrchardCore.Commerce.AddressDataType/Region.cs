a571e7ef4ada17618a0807acfbab27107459c1f6
Krisztián Németh <krisztian.nemeth@lombiq.com>
2024-10-04 13:36:14 +0200

    Adding json constructor

--- src/Libraries/OrchardCore.Commerce.AddressDataType/Region.cs
+++ src/Libraries/OrchardCore.Commerce.AddressDataType/Region.cs
@@ -1,9 +1,17 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
+using System.Text.Json.Serialization;
 
 namespace OrchardCore.Commerce.AddressDataType;
 
 public record Region(string EnglishName, string TwoLetterISORegionName, string DisplayName)
 {
+    [Obsolete("Only serializers should use this.")]
+    [JsonConstructor]
+    private Region()
+        : this(string.Empty, string.Empty, string.Empty)
+    { }
+
     public Region(RegionInfo info)
         : this(info.EnglishName, info.TwoLetterISORegionName, info.EnglishName)
     { }
