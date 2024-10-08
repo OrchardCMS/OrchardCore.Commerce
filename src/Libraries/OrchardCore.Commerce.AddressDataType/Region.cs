bea5d761077d02d373e4f2d635a68a5af44b562d
Sára El-Saig <sara.el-saig@lombiq.com>
2024-10-08 23:02:52 +0200

    Bug fix.

--- src/Libraries/OrchardCore.Commerce.AddressDataType/Region.cs
+++ src/Libraries/OrchardCore.Commerce.AddressDataType/Region.cs
@@ -4,7 +4,9 @@ namespace OrchardCore.Commerce.AddressDataType;
 
 public record Region(string EnglishName, string TwoLetterISORegionName, string DisplayName)
 {
-    public Region(RegionInfo info)
-        : this(info.EnglishName, info.TwoLetterISORegionName, info.EnglishName)
-    { }
+    // This used to be a constructor, but has been turned into a factory method to avoid causing JSON serialization
+    // exceptions. Seehttps://github.com/dotnet/runtime/issues/45373#issuecomment-812091894 for more info on the
+    // problem in general.
+    public static Region FromRegionInfo(RegionInfo info) =>
+        new(info.EnglishName, info.TwoLetterISORegionName, info.EnglishName);
 }
