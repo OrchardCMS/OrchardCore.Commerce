e0cfbeda59dfaee6982f10e04280810a74d3b657
Krisztián Németh <krisztian.nemeth@lombiq.com>
2024-11-28 16:25:40 +0100

    Not needed using

--- src/Libraries/OrchardCore.Commerce.MoneyDataType/Amount.cs
+++ src/Libraries/OrchardCore.Commerce.MoneyDataType/Amount.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
-using System.Linq;
 using System.Text.Json.Serialization;
 
 namespace OrchardCore.Commerce.MoneyDataType;
