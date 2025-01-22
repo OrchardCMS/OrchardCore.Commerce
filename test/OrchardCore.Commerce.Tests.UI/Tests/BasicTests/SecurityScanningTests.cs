a0bdb28ec90d28bf251d6765d05fc07c2287c6e8
Sára El-Saig <sara.el-saig@lombiq.com>
2025-01-22 21:14:00 +0100

    Update FullSecurityScanShouldPass.

--- test/OrchardCore.Commerce.Tests.UI/Tests/BasicTests/SecurityScanningTests.cs
+++ test/OrchardCore.Commerce.Tests.UI/Tests/BasicTests/SecurityScanningTests.cs
@@ -39,18 +39,10 @@ public class SecurityScanningTests : UITestBase
                 },
                 maxActiveScanDurationInMinutes: 5,
                 maxRuleDurationInMinutes: 1),
-            changeConfiguration: configuration => configuration.AssertAppLogsAsync = async webApplicationInstance =>
-            {
-                var logsWithoutUnwantedExceptionMessages = (await webApplicationInstance.GetLogOutputAsync())
-                    .SplitByNewLines()
-                    .Where(message =>
-                        !message.ContainsOrdinalIgnoreCase("System.IO.DirectoryNotFoundException: Could not find a part of the path") &&
-                        !message.ContainsOrdinalIgnoreCase(
-                            "System.IO.IOException: The filename, directory name, or volume label syntax is incorrect") &&
-                        !message.ContainsOrdinalIgnoreCase("System.InvalidOperationException: This action intentionally causes an exception!"));
-
-                logsWithoutUnwantedExceptionMessages.ShouldNotContain(item => item.Contains("|ERROR|"));
-            });
+            changeConfiguration: configuration => configuration.UseAssertAppLogsForSecurityScan(
+                "System.IO.DirectoryNotFoundException: Could not find a part of the path",
+                "System.IO.IOException: The filename, directory name, or volume label syntax is incorrect",
+                "System.InvalidOperationException: This action intentionally causes an exception!"));
 
     private static void FalsePositive(
         SecurityScanConfiguration configuration,
