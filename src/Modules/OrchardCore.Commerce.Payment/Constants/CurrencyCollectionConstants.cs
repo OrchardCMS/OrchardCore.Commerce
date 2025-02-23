0035bc24298c50fd98a879ee5785b5ec6cb2ebcf
Sára El-Saig <sara.el-saig@lombiq.com>
2025-02-23 08:34:52 +0100

    Clarification.

--- src/Modules/OrchardCore.Commerce.Payment/Constants/CurrencyCollectionConstants.cs
+++ src/Modules/OrchardCore.Commerce.Payment/Constants/CurrencyCollectionConstants.cs
@@ -4,6 +4,10 @@ namespace OrchardCore.Commerce.Payment.Constants;
 public static class CurrencyCollectionConstants
 {
     public static readonly IEnumerable<string> SpecialCases = ["ISK", "HUF", "TWD", "UGX"];
+
+    // Note that https://docs.stripe.com/currencies#zero-decimal contains UGX on the list as well, however this is a
+    // mistake because https://docs.stripe.com/currencies#special-cases explicitly states that UGX is a special case
+    // (see above) where it's effectively zero-decimal but uses the two-decimal format for backwards compatibility.
     public static readonly IEnumerable<string> ZeroDecimalCurrencies =
     [
         "BIF",
