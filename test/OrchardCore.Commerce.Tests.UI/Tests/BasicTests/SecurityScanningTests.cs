0ce5a3af5e59e3b98b11735c700b50ae366b7f6f
Sára El-Saig <sara.el-saig@lombiq.com>
2025-03-05 00:13:15 +0100

    Revert OS and github check.

--- test/OrchardCore.Commerce.Tests.UI/Tests/BasicTests/SecurityScanningTests.cs
+++ test/OrchardCore.Commerce.Tests.UI/Tests/BasicTests/SecurityScanningTests.cs
@@ -1,5 +1,4 @@
 using Lombiq.Tests.UI.SecurityScanning;
-using Lombiq.Tests.UI.Services.GitHub;
 using Xunit;
 
 namespace OrchardCore.Commerce.Tests.UI.Tests.BasicTests;
@@ -13,38 +12,36 @@ public class SecurityScanningTests : UITestBase
 
     [Fact]
     public Task FullSecurityScanShouldPass() =>
-        OperatingSystem.IsWindows() && GitHubHelper.IsGitHubEnvironment
-            ? Task.CompletedTask
-            : ExecuteTestAfterSetupAsync(
-                context => context.RunAndConfigureAndAssertFullSecurityScanForContinuousIntegrationAsync(
-                    configuration =>
-                    {
-                        configuration.DisableActiveScanRule(
-                            6,
-                            "Path Traversal (all paths are virtual so it's not a real concern, also creates too many errors)");
+        ExecuteTestAfterSetupAsync(
+            context => context.RunAndConfigureAndAssertFullSecurityScanForContinuousIntegrationAsync(
+                configuration =>
+                {
+                    configuration.DisableActiveScanRule(
+                        6,
+                        "Path Traversal (all paths are virtual so it's not a real concern, also creates too many errors)");
 
-                        configuration.DisableActiveScanRule(
-                            40024,
-                            "SQL Injection - SQLite (everything goes through YesSql so these are false positive)");
+                    configuration.DisableActiveScanRule(
+                        40024,
+                        "SQL Injection - SQLite (everything goes through YesSql so these are false positive)");
 
-                        configuration.DisableActiveScanRule(
-                            40027,
-                            "The query time is controllable using parameter value [some SQL injection]");
+                    configuration.DisableActiveScanRule(
+                        40027,
+                        "The query time is controllable using parameter value [some SQL injection]");
 
-                        FalsePositive(
-                            configuration,
-                            10202,
-                            "Absence of Anti-CSRF Tokens",
-                            "The ProductListPart-Filters intentionally uses a GET form. No XSS risk.",
-                            @"https://[^/]+/",
-                            @".*/\?.*pagenum=.*",
-                            @".*/\?.*products\..*");
+                    FalsePositive(
+                        configuration,
+                        10202,
+                        "Absence of Anti-CSRF Tokens",
+                        "The ProductListPart-Filters intentionally uses a GET form. No XSS risk.",
+                        @"https://[^/]+/",
+                        @".*/\?.*pagenum=.*",
+                        @".*/\?.*products\..*");
 
-                        // Not relevant for testing OCC.
-                        configuration.DontScanErrorPage = true;
-                    },
-                    maxActiveScanDurationInMinutes: 5,
-                    maxRuleDurationInMinutes: 1));
+                    // Not relevant for testing OCC.
+                    configuration.DontScanErrorPage = true;
+                },
+                maxActiveScanDurationInMinutes: 5,
+                maxRuleDurationInMinutes: 1));
 
     private static void FalsePositive(
         SecurityScanConfiguration configuration,
