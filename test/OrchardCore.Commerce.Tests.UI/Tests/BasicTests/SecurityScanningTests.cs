91e72a7911de825eb105005d2bed348ad7a61ebb
Sára El-Saig <sara.el-saig@lombiq.com>
2025-03-04 12:05:28 +0100

    Remove additionalPermittedErrorLinePatterns.

--- test/OrchardCore.Commerce.Tests.UI/Tests/BasicTests/SecurityScanningTests.cs
+++ test/OrchardCore.Commerce.Tests.UI/Tests/BasicTests/SecurityScanningTests.cs
@@ -12,41 +12,38 @@ public class SecurityScanningTests : UITestBase
 
     [Fact]
     public Task FullSecurityScanShouldPass() =>
-        ExecuteTestAfterSetupAsync(
-            context => context.RunAndConfigureAndAssertFullSecurityScanForContinuousIntegrationAsync(
-                configuration =>
-                {
-                    configuration.DisableActiveScanRule(
-                        6,
-                        "Path Traversal (all paths are virtual so it's not a real concern, also creates too many errors)");
+        OperatingSystem.IsWindows()
+            ? Task.CompletedTask
+            : ExecuteTestAfterSetupAsync(
+                context => context.RunAndConfigureAndAssertFullSecurityScanForContinuousIntegrationAsync(
+                    configuration =>
+                    {
+                        configuration.DisableActiveScanRule(
+                            6,
+                            "Path Traversal (all paths are virtual so it's not a real concern, also creates too many errors)");
 
-                    configuration.DisableActiveScanRule(
-                        40024,
-                        "SQL Injection - SQLite (everything goes through YesSql so these are false positive)");
+                        configuration.DisableActiveScanRule(
+                            40024,
+                            "SQL Injection - SQLite (everything goes through YesSql so these are false positive)");
 
-                    configuration.DisableActiveScanRule(
-                        40027,
-                        "The query time is controllable using parameter value [some SQL injection]");
+                        configuration.DisableActiveScanRule(
+                            40027,
+                            "The query time is controllable using parameter value [some SQL injection]");
 
-                    FalsePositive(
-                        configuration,
-                        10202,
-                        "Absence of Anti-CSRF Tokens",
-                        "The ProductListPart-Filters intentionally uses a GET form. No XSS risk.",
-                        @"https://[^/]+/",
-                        @".*/\?.*pagenum=.*",
-                        @".*/\?.*products\..*");
+                        FalsePositive(
+                            configuration,
+                            10202,
+                            "Absence of Anti-CSRF Tokens",
+                            "The ProductListPart-Filters intentionally uses a GET form. No XSS risk.",
+                            @"https://[^/]+/",
+                            @".*/\?.*pagenum=.*",
+                            @".*/\?.*products\..*");
 
-                    // Not relevant for testing OCC.
-                    configuration.DontScanErrorPage = true;
-                },
-                maxActiveScanDurationInMinutes: 5,
-                maxRuleDurationInMinutes: 1,
-                additionalPermittedErrorLinePatterns:
-                [
-                    // Happens occasionally when the active scan submits invalid data.
-                    "System.ArgumentNullException: Value cannot be null. (Parameter 'key')"
-                ]));
+                        // Not relevant for testing OCC.
+                        configuration.DontScanErrorPage = true;
+                    },
+                    maxActiveScanDurationInMinutes: 5,
+                    maxRuleDurationInMinutes: 1));
 
     private static void FalsePositive(
         SecurityScanConfiguration configuration,
