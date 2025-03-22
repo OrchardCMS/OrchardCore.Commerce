505e2a55fdc076f5f2090e8d614da478770af14d
infofromca <zgofmontreal@gmail.com>
2025-03-22 12:33:54 -0400

    using SystemEnvironment

--- src/Modules/OrchardCore.Commerce/Endpoints/Extensions/ConvertLocalizedHtmlString.cs
+++ src/Modules/OrchardCore.Commerce/Endpoints/Extensions/ConvertLocalizedHtmlString.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Mvc.Localization;
 using System.Collections.Generic;
 using System.Linq;
+using SystemEnvironment = System.Environment;
 
 namespace OrchardCore.Commerce.Endpoints.Extensions;
+
 public static class ConvertLocalizedHtmlString
 {
     public static string ConvertLocalizedHtmlStringList(this IReadOnlyList<LocalizedHtmlString> errors) =>
-        string.Join(System.Environment.NewLine, errors.Select(error => error.Html()));
+        string.Join(SystemEnvironment.NewLine, errors.Select(error => error.Html()));
 }
