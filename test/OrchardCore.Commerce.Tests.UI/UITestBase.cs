01a9f9fae0a4674816061cfbe76d0a788a0c869c
Jasmin Savard <jasmin.savard@gmail.com>
2025-05-06 21:59:43 -0400

    internal sealed

--- test/OrchardCore.Commerce.Tests.UI/UITestBase.cs
+++ test/OrchardCore.Commerce.Tests.UI/UITestBase.cs
@@ -5,35 +5,43 @@ using Xunit;
 
 namespace OrchardCore.Commerce.Tests.UI;
 
-internal class UITestBase : OrchardCoreUITestBase<Program>
+#pragma warning disable CA1515 // Consider making public types internal
+public class UITestBase : OrchardCoreUITestBase<Program>
+#pragma warning restore CA1515 // Consider making public types internal
 {
     protected UITestBase(ITestOutputHelper testOutputHelper)
-        : base(testOutputHelper)
-    {
-    }
+        : base(testOutputHelper) { }
 
     protected override Task ExecuteTestAfterSetupAsync(
         Func<UITestContext, Task> testAsync,
         Browser browser,
-        Func<OrchardCoreUITestExecutorConfiguration, Task> changeConfigurationAsync) =>
-        ExecuteTestAfterSetupAsync(testAsync, browser, changeConfigurationAsync, timeout: null);
+        Func<OrchardCoreUITestExecutorConfiguration, Task> changeConfigurationAsync
+    ) => ExecuteTestAfterSetupAsync(testAsync, browser, changeConfigurationAsync, timeout: null);
 
     protected async Task ExecuteTestAfterSetupAsync(
         Func<UITestContext, Task> testAsync,
         Browser browser,
         Func<OrchardCoreUITestExecutorConfiguration, Task> changeConfigurationAsync,
-        TimeSpan? timeout)
+        TimeSpan? timeout
+    )
     {
         var timeoutValue = timeout ?? TimeSpan.FromMinutes(10);
 
-        var testTask = ExecuteTestAsync(testAsync, browser, SetupHelpers.RunSetupAsync, changeConfigurationAsync);
+        var testTask = ExecuteTestAsync(
+            testAsync,
+            browser,
+            SetupHelpers.RunSetupAsync,
+            changeConfigurationAsync
+        );
         var timeoutTask = Task.Delay(timeoutValue, CancellationToken.None);
 
         await Task.WhenAny(testTask, timeoutTask);
 
         if (timeoutTask.IsCompleted)
         {
-            throw new TimeoutException($"The time allotted for the test ({timeoutValue}) was exceeded.");
+            throw new TimeoutException(
+                $"The time allotted for the test ({timeoutValue}) was exceeded."
+            );
         }
 
         await testTask;
@@ -43,19 +51,25 @@ internal class UITestBase : OrchardCoreUITestBase<Program>
         Func<UITestContext, Task> testAsync,
         Browser browser,
         Func<UITestContext, Task<Uri>> setupOperation,
-        Func<OrchardCoreUITestExecutorConfiguration, Task> changeConfigurationAsync) =>
+        Func<OrchardCoreUITestExecutorConfiguration, Task> changeConfigurationAsync
+    ) =>
         base.ExecuteTestAsync(
             testAsync,
             browser,
             setupOperation,
             async configuration =>
             {
-                configuration.AccessibilityCheckingConfiguration.RunAccessibilityCheckingAssertionOnAllPageChanges = true;
-                configuration.AccessibilityCheckingConfiguration.AxeBuilderConfigurator += axeBuilder =>
-                    AccessibilityCheckingConfiguration
-                        .ConfigureWcag21aa(axeBuilder)
-                        .DisableRules("color-contrast");
+                configuration
+                    .AccessibilityCheckingConfiguration
+                    .RunAccessibilityCheckingAssertionOnAllPageChanges = true;
+                configuration.AccessibilityCheckingConfiguration.AxeBuilderConfigurator +=
+                    axeBuilder =>
+                        AccessibilityCheckingConfiguration
+                            .ConfigureWcag21aa(axeBuilder)
+                            .DisableRules("color-contrast");
 
-                if (changeConfigurationAsync != null) await changeConfigurationAsync(configuration);
-            });
+                if (changeConfigurationAsync != null)
+                    await changeConfigurationAsync(configuration);
+            }
+        );
 }
