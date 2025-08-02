274d89382b27c61ca9c17df960d868662ad1db0c
infofromca <zgofmontreal@gmail.com>
2025-08-02 12:00:12 -0400

    Rename to ManageAddressDisplaySettings

--- src/Modules/OrchardCore.Commerce.Payment/Permissions.cs
+++ src/Modules/OrchardCore.Commerce.Payment/Permissions.cs
@@ -8,9 +8,9 @@ public class Permissions : AdminPermissionBase
 {
     public static readonly Permission ManageOrders = new(nameof(ManageOrders), "Manage Orders");
     public static readonly Permission Checkout = new(nameof(Checkout), "Ability to checkout");
-    public static readonly Permission ManageAddressSettings = new(nameof(ManageAddressSettings), "Manage Address Settings");
+    public static readonly Permission ManageAddressDisplaySettings = new(nameof(ManageAddressDisplaySettings), "Manage Address Settings");
 
-    private static readonly IReadOnlyList<Permission> _adminPermissions = [ManageOrders, Checkout, ManageAddressSettings];
+    private static readonly IReadOnlyList<Permission> _adminPermissions = [ManageOrders, Checkout, ManageAddressDisplaySettings];
 
     protected override IEnumerable<Permission> AdminPermissions => _adminPermissions;
 }
