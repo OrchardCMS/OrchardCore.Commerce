d7e02b33b03bc1d72dcb3ee55fedd041893d946e
Jasmin Savard <3228637+Skrypt@users.noreply.github.com>
2025-08-18 07:12:02 -0400

    Stripe Connect (#621)
    * Stripe Connect
    
    Add stripe connect Account Id for connected accounts.
    
    * using order
    
    * Update hint
    
    * null check
    
    * Update src/Modules/OrchardCore.Commerce.Payment.Stripe/Views/StripeApiSettings.Edit.cshtml
    
    Co-authored-by: Sára El-Saig <sara.el-saig@lombiq.com>
    
    * Update src/Modules/OrchardCore.Commerce.Payment.Stripe/Views/CheckoutStripe.cshtml
    
    Co-authored-by: Sára El-Saig <sara.el-saig@lombiq.com>
    
    * suggested changes
    
    * Update src/Modules/OrchardCore.Commerce.Payment.Stripe/Services/RequestOptionsService.cs
    
    Co-authored-by: Sára El-Saig <sara.el-saig@lombiq.com>
    
    * format
    
    * sort using
    
    ---------
    
    Co-authored-by: Sára El-Saig <sara.el-saig@lombiq.com>

--- src/Modules/OrchardCore.Commerce.Payment.Stripe/Models/StripePaymentProviderData.cs
+++ src/Modules/OrchardCore.Commerce.Payment.Stripe/Models/StripePaymentProviderData.cs
@@ -1,8 +1,11 @@
+#nullable enable
+
 namespace OrchardCore.Commerce.Payment.Stripe.Models;
 
 public class StripePaymentProviderData
 {
-    public string PublishableKey { get; set; }
-    public string ClientSecret { get; set; }
-    public string PaymentIntentId { get; set; }
+    public required string PublishableKey { get; set; }
+    public required string ClientSecret { get; set; }
+    public string? AccountId { get; set; }
+    public required string PaymentIntentId { get; set; }
 }
