714c21493ed1ca315095ff50c795c3559a2e5874
Sára El-Saig <sara.el-saig@lombiq.com>
2025-10-14 17:49:52 +0200

    unusing

--- src/Modules/OrchardCore.Commerce.Tax/Drivers/TaxRateSettingsDisplayDriver.cs
+++ src/Modules/OrchardCore.Commerce.Tax/Drivers/TaxRateSettingsDisplayDriver.cs
@@ -11,7 +11,6 @@ using OrchardCore.DisplayManagement.Views;
 using OrchardCore.Settings;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
