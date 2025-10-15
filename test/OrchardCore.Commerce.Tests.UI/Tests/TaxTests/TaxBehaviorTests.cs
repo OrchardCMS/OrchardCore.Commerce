30d744066104f0f6751b16476963a777baff662b
Sára El-Saig <sara.el-saig@lombiq.com>
2025-10-15 22:38:11 +0200

    Revert "Verify if this fails on GHA as well."
    This reverts commit e0ea59fe392c65b19a0a1b5ad7704b2acf1e4d28.

--- test/OrchardCore.Commerce.Tests.UI/Tests/TaxTests/TaxBehaviorTests.cs
+++ test/OrchardCore.Commerce.Tests.UI/Tests/TaxTests/TaxBehaviorTests.cs
@@ -141,10 +141,13 @@ public class TaxBehaviorTests : UITestBase
                         ByCell(index, nameof(TaxRateSetting.IsCorporation)),
                         value.ToString());
 
-                Task SetTaxRateAsync(int index, decimal value) =>
-                    context.ClickAndFillInWithRetriesAsync(
-                        ByCell(index, nameof(TaxRateSetting.TaxRate)),
-                        value.ToTechnicalString());
+                void SetTaxRate(int index, decimal value)
+                {
+                    var element = context.Get(ByCell(index, nameof(TaxRateSetting.TaxRate)));
+                    element.Click();
+                    element.Clear();
+                    element.SendKeysWithLogging(value.ToTechnicalString());
+                }
 
                 // Enable feature.
                 await context.SignInDirectlyAsync();
@@ -166,7 +169,7 @@ public class TaxBehaviorTests : UITestBase
                 await SetCellAsync(0, nameof(TaxRateSetting.DestinationRegion), "CA[");
                 await SetCellAsync(0, nameof(TaxRateSetting.TaxCode), "TVQ");
                 await SetCellAsync(0, nameof(TaxRateSetting.VatNumber), "1");
-                await SetTaxRateAsync(0, 0.01m);
+                SetTaxRate(0, 0.01m);
                 await SetCorporationAsync(0, MatchTaxRates.Checked);
                 ResetScroll();
 
@@ -175,7 +178,7 @@ public class TaxBehaviorTests : UITestBase
                 await SetCellAsync(1, nameof(TaxRateSetting.DestinationCity), "Budapest");
                 await SetCellAsync(1, nameof(TaxRateSetting.DestinationPostalCode), "][");
                 await SetCellAsync(1, nameof(TaxRateSetting.DestinationRegion), "HU");
-                await SetTaxRateAsync(1, 27);
+                SetTaxRate(1, 27);
                 ResetScroll();
 
                 // Click "Save", error message should be displayed after page load.
