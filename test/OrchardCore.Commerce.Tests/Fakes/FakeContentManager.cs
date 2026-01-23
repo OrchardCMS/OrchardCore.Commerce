d21a211e316e5e086b0e9646f56ba507ba649539
Sára El-Saig <sara.el-saig@lombiq.com>
2026-01-23 23:05:53 +0100

    Fix build errors.

--- test/OrchardCore.Commerce.Tests/Fakes/FakeContentManager.cs
+++ test/OrchardCore.Commerce.Tests/Fakes/FakeContentManager.cs
@@ -6,59 +6,72 @@ using System.Threading.Tasks;
 
 namespace OrchardCore.Commerce.Tests.Fakes;
 
-public class FakeContentManager : IContentManager
+public class FakeContentManager : NullContentManager
 {
-    public Task<ContentItem> CloneAsync(ContentItem contentItem) => throw new NotSupportedException();
-
-    public Task CreateAsync(ContentItem contentItem, VersionOptions options = null) =>
-        throw new NotSupportedException();
-
-    public Task<ContentValidateResult> CreateContentItemVersionAsync(ContentItem contentItem) =>
-        throw new NotSupportedException();
-
-    public Task DiscardDraftAsync(ContentItem contentItem) => throw new NotSupportedException();
-
-    public Task<ContentItem> GetAsync(string contentItemId, VersionOptions options = null) => throw new NotSupportedException();
-
-    public Task<IEnumerable<ContentItem>> GetAsync(IEnumerable<string> contentItemIds, bool latest = false) =>
-        throw new NotSupportedException();
-
-    public Task<IEnumerable<ContentItem>> GetAsync(IEnumerable<string> contentItemIds, VersionOptions options = null) =>
-        throw new NotSupportedException();
-
-    public Task<ContentItem> GetVersionAsync(string contentItemVersionId) => throw new NotSupportedException();
-    public Task<IEnumerable<ContentItem>> GetAllVersionsAsync(string contentItemId) => throw new NotSupportedException();
-
-    public Task ImportAsync(IEnumerable<ContentItem> contentItems) => throw new NotSupportedException();
-
-    public Task<ContentItem> LoadAsync(ContentItem contentItem) => throw new NotSupportedException();
-
-    public Task<ContentItem> NewAsync(string contentType) => throw new NotSupportedException();
-
-    public Task<TAspect> PopulateAspectAsync<TAspect>(IContent content, TAspect aspect)
+    public override Task<TAspect> PopulateAspectAsync<TAspect>(IContent content, TAspect aspect)
     {
         if (typeof(TAspect) != typeof(ContentItemMetadata)) throw new NotSupportedException();
-        var metadata = new ContentItemMetadata
-        {
-            DisplayRouteValues = [],
-        };
+
+        var metadata = new ContentItemMetadata { DisplayRouteValues = [] };
         return Task.FromResult((TAspect)(object)metadata);
     }
+}
 
-    public Task PublishAsync(ContentItem contentItem) => throw new NotSupportedException();
+public class NullContentManager : IContentManager
+{
+    public virtual Task<ContentItem> NewAsync(string contentType) => Task.FromResult(new ContentItem());
 
-    public Task RemoveAsync(ContentItem contentItem) => throw new NotSupportedException();
+    public virtual Task UpdateAsync(ContentItem contentItem) => Task.CompletedTask;
 
-    public Task<ContentValidateResult> RestoreAsync(ContentItem contentItem) => throw new NotSupportedException();
+    public virtual Task<bool> CreateAsync(ContentItem contentItem, VersionOptions options = null) =>
+        Task.FromResult(false);
 
-    public Task SaveDraftAsync(ContentItem contentItem) => throw new NotSupportedException();
+    public virtual Task<ContentValidateResult> CreateContentItemVersionAsync(ContentItem contentItem) =>
+        Task.FromResult(new ContentValidateResult());
 
-    public Task UnpublishAsync(ContentItem contentItem) => throw new NotSupportedException();
+    public virtual Task<ContentValidateResult> UpdateContentItemVersionAsync(
+        ContentItem updatingVersion,
+        ContentItem updatedVersion) =>
+        Task.FromResult(new ContentValidateResult());
 
-    public Task UpdateAsync(ContentItem contentItem) => throw new NotSupportedException();
+    public virtual Task ImportAsync(IEnumerable<ContentItem> contentItems) => Task.CompletedTask;
 
-    public Task<ContentValidateResult> UpdateContentItemVersionAsync(ContentItem updatingVersion, ContentItem updatedVersion) =>
-        throw new NotSupportedException();
+    public virtual Task<ContentValidateResult> ValidateAsync(ContentItem contentItem) =>
+        Task.FromResult(new ContentValidateResult());
 
-    public Task<ContentValidateResult> ValidateAsync(ContentItem contentItem) => throw new NotSupportedException();
+    public virtual Task<ContentValidateResult> RestoreAsync(ContentItem contentItem) =>
+        Task.FromResult(new ContentValidateResult());
+
+    public virtual Task<ContentItem> GetAsync(string contentItemId, VersionOptions options = null) =>
+        Task.FromResult(new ContentItem());
+
+    public virtual Task<IEnumerable<ContentItem>> GetAsync(
+        IEnumerable<string> contentItemIds,
+        VersionOptions options = null) =>
+        Task.FromResult<IEnumerable<ContentItem>>([]);
+
+    public virtual Task<ContentItem> GetVersionAsync(string contentItemVersionId) =>
+        Task.FromResult(new ContentItem());
+
+    public virtual Task<IEnumerable<ContentItem>> GetAllVersionsAsync(string contentItemId) =>
+        Task.FromResult<IEnumerable<ContentItem>>([]);
+
+    public virtual Task<ContentItem> LoadAsync(ContentItem contentItem) =>
+        Task.FromResult(new ContentItem());
+
+    public virtual Task RemoveAsync(ContentItem contentItem) => Task.CompletedTask;
+
+    public virtual Task DiscardDraftAsync(ContentItem contentItem) => Task.CompletedTask;
+
+    public virtual Task SaveDraftAsync(ContentItem contentItem) => Task.CompletedTask;
+
+    public virtual Task<bool> PublishAsync(ContentItem contentItem) => Task.FromResult(false);
+
+    public virtual Task<bool> UnpublishAsync(ContentItem contentItem) => Task.FromResult(false);
+
+    public virtual Task<TAspect> PopulateAspectAsync<TAspect>(IContent content, TAspect aspect) =>
+        Task.FromResult<TAspect>(default);
+
+    public virtual Task<ContentItem> CloneAsync(ContentItem contentItem) =>
+        Task.FromResult(new ContentItem());
 }
