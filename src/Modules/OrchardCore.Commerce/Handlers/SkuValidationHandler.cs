3cd4e465dbe17f6c5aa5ea6bc477a2f6b14dbce2
Sára El-Saig <sara.el-saig@lombiq.com>
2026-03-22 20:11:15 +0100

    Fill SKU value.

--- src/Modules/OrchardCore.Commerce/Handlers/SkuValidationHandler.cs
+++ src/Modules/OrchardCore.Commerce/Handlers/SkuValidationHandler.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Extensions.Localization;
+using OrchardCore.Commerce.Abstractions.Abstractions;
 using OrchardCore.Commerce.Indexes;
 using OrchardCore.Commerce.Models;
 using OrchardCore.Commerce.Services;
 using OrchardCore.ContentManagement;
+using OrchardCore.ContentManagement.Handlers;
 using OrchardCore.DisplayManagement.ModelBinding;
 using System;
 using System.Collections.Generic;
@@ -12,26 +14,45 @@ using YesSql;
 
 namespace OrchardCore.Commerce.Handlers;
 
-public class SkuValidationHandler : CreatingOrUpdatingPartHandler<ProductPart>
+public class SkuValidationHandler : ContentPartHandler<ProductPart>
 {
     private readonly ISession _session;
     private readonly IUpdateModelAccessor _updateModelAccessor;
     private readonly IEnumerable<IDuplicateSkuResolver> _duplicateSkuResolvers;
+    private readonly IEnumerable<ISkuGenerator> _skuGenerators;
     private readonly IStringLocalizer<SkuValidationHandler> T;
 
     public SkuValidationHandler(
         ISession session,
         IUpdateModelAccessor updateModelAccessor,
         IEnumerable<IDuplicateSkuResolver> duplicateSkuResolvers,
+        IEnumerable<ISkuGenerator> skuGenerators,
         IStringLocalizer<SkuValidationHandler> stringLocalizer)
     {
         _session = session;
         _updateModelAccessor = updateModelAccessor;
         _duplicateSkuResolvers = duplicateSkuResolvers;
+        _skuGenerators = skuGenerators;
         T = stringLocalizer;
     }
 
-    protected override async Task CreatingOrUpdatingAsync(ProductPart part)
+    public override async Task CreatingAsync(CreateContentContext context, ProductPart part)
+    {
+        // If we have an SKU generator and the SKU is either empty or it must not be manually filled, then overwrite it
+        // with the generated value. 
+        if (_skuGenerators.HighestPriority() is { } generator &&
+            (string.IsNullOrWhiteSpace(part.Sku) || !generator.IsManualAllowed))
+        {
+            part.Sku = await generator.GenerateSkuAsync(part.ContentItem);
+        }
+        
+        await CreatingOrUpdatingAsync(part);
+    }
+
+    public override Task UpdatingAsync(UpdateContentContext context, ProductPart part) =>
+        CreatingOrUpdatingAsync(part);
+
+    private async Task CreatingOrUpdatingAsync(ProductPart part)
     {
         if (string.IsNullOrWhiteSpace(part.Sku))
         {
@@ -47,14 +68,14 @@ public class SkuValidationHandler : CreatingOrUpdatingPartHandler<ProductPart>
             .AsList();
 
         var resolvers = _duplicateSkuResolvers.AsList();
-        for (var i = 0; i < resolvers.Count && alreadyExisting?.Any() == true; i++)
+        for (var i = 0; i < resolvers.Count && alreadyExisting.Any(); i++)
         {
             alreadyExisting =
                 await resolvers[i].UpdateDuplicatesListAsync(part.ContentItem, alreadyExisting) ??
                 Array.Empty<ContentItem>();
         }
 
-        if (alreadyExisting?.Any() == true)
+        if (alreadyExisting.Any())
         {
             _updateModelAccessor.ModelUpdater.ModelState.AddModelError(
                 nameof(part.Sku),
