cbb1a00745794d2816a1b13e3485a42b8be1cbaf
Sára El-Saig <sara.el-saig@lombiq.com>
2026-03-23 16:05:58 +0100

    Only uppercase if not read only.

--- src/Modules/OrchardCore.Commerce/Drivers/ProductPartDisplayDriver.cs
+++ src/Modules/OrchardCore.Commerce/Drivers/ProductPartDisplayDriver.cs
@@ -56,12 +56,14 @@ public class ProductPartDisplayDriver : ContentPartDisplayDriver<ProductPart>
             return await EditAsync(part, context);
         }
 
-        part.Sku = part.Sku.ToUpperInvariant();
-
         // If the SKU is read-only then editing should not be possible, but here we undo any POST trickery just in case.
         if (IsSkuReadOnly)
         {
             part.Sku = skuBefore;
+        }
+        else
+        {
+            part.Sku = part.Sku.ToUpperInvariant();
         }
 
         if (part.ContentItem.As<InventoryPart>() is { } inventoryPart)
