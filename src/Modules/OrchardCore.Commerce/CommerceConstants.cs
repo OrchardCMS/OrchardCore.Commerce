fa368cb1c12afa1088ddb0cd7de0e3fcea419f16
Sára El-Saig <sara.el-saig@lombiq.com>
2026-03-25 15:49:01 +0100

    OCC-431: Add SKU generator interface and optional GUID SKU feature (#722)
    * Update All Dependencies
    
    * Update OC preview and fix build errors.
    
    * Update OC preview.
    
    * Update OC preview.
    
    * Update OC preview.
    
    * Update Lombiq nugets.
    
    * Update docker files.
    
    * Fix Dockerfile.
    
    * Block false positive.
    
    * Use MediaTypeNames.
    
    * Fix package downgrades.
    
    * Add ISkuGenerator.
    
    * Clean up features.
    
    * Handle SKU generator read-only flag in the product driver.
    
    * Add GuidSkuGenerator.
    
    * Add docs.
    
    * Fill SKU value.
    
    * nuget
    
    * Fix HL NuGet.
    
    * HL loading hotfix.
    
    * Fix NRE.
    
    * Fix generated SKU not actually getting saved.
    
    * Add UI Test.
    
    * Fix SA1028.
    
    * Fix SA1028.
    
    * Fix IDE0022.
    
    * Fix IDE0055, SA1028.
    
    * Only uppercase if not read only.
    
    * Fix S3240.
    
    ---------
    
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- src/Modules/OrchardCore.Commerce/CommerceConstants.cs
+++ src/Modules/OrchardCore.Commerce/CommerceConstants.cs
@@ -5,8 +5,10 @@ public static class CommerceConstants
     public static class Features
     {
         public const string Core = "OrchardCore.Commerce";
-        public const string SessionCartStorage = "OrchardCore.Commerce.SessionCartStorage";
-        public const string CurrencySettingsSelector = "OrchardCore.Commerce.CurrencySettingsSelector";
-        public const string Subscription = "OrchardCore.Commerce.Subscription";
+        public const string SkuGenerator = $"{Core}.{nameof(SkuGenerator)}";
+        public const string SessionCartStorage = $"{Core}.{nameof(SessionCartStorage)}";
+        public const string CurrencySettingsSelector = $"{Core}.{nameof(CurrencySettingsSelector)}";
+        public const string Subscription = $"{Core}.{nameof(Subscription)}";
+        public const string SkuGeneratorGuid = $"{SkuGenerator}.Guid";
     }
 }
