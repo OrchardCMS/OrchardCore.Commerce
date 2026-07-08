913fef75df732466c1108a74b43a97cd2728765c
andy <zgofmontreal@gmail.com>
2026-07-08 13:21:49 -0400

    Update src/Modules/OrchardCore.Commerce/Handlers/SkuValidationHandler.cs
    Co-authored-by: Sára El-Saig <sara.el-saig@lombiq.com>

--- src/Modules/OrchardCore.Commerce/Handlers/SkuValidationHandler.cs
+++ src/Modules/OrchardCore.Commerce/Handlers/SkuValidationHandler.cs
@@ -43,6 +43,7 @@ public class SkuValidationHandler : ContentPartHandler<ProductPart>
     public override async Task CreatingAsync(CreateContentContext context, ProductPart part)
     {
         var skuBefore = part.Sku ?? string.Empty;
+
         // If we have an SKU generator and the SKU is either empty or it must not be manually filled, then overwrite it
         // with the generated value.
         if (_skuGenerators.HighestPriority() is { } generator &&
